049f1633b34386f08eea2f5324453010dc4b43cd
bates <unknown@noreply.com>
2004-04-17 16:58:16 +0000

    Cleanup of utilities
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/matrix@98 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- branches/trunk-lme4/src/utilities.c
+++ branches/trunk-lme4/src/utilities.c
@@ -11,19 +11,18 @@
  * @return obj, with the named slot modified in place.
  */
 SEXP
-nlme_replaceSlot(SEXP obj, const SEXPREC* names, const SEXPREC* value)
+nlme_replaceSlot(SEXP obj, SEXP names, SEXP value)
 {
-    int lnm1 = length((SEXP) names) - 1;
+    int lnm1 = length(names) - 1;
 
     if (lnm1 >= 0) {
 	SEXP comp = obj;
 	int i;
 
 	for (i = 0; i < lnm1; i++) {
-	    comp = GET_SLOT(comp, install(CHAR(STRING_ELT((SEXP) names, i))));
+	    comp = GET_SLOT(comp, install(CHAR(STRING_ELT(names, i))));
 	}
-	SET_SLOT(comp, install(CHAR(STRING_ELT((SEXP) names, lnm1))),
-		 (SEXP) value);
+	SET_SLOT(comp, install(CHAR(STRING_ELT(names, lnm1))), value);
     }
     return obj;
 }
