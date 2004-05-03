859b5c01a230d71da3461a051f7a82f829481c71
bates <unknown@noreply.com>
2004-05-03 12:49:17 +0000

    Rearranging code to isolate Metis includes
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/matrix@148 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/src/Metis_utils.h
+++ pkg/src/Metis_utils.h
@@ -7,8 +7,5 @@
 void ssc_metis_order(int n, const int Tp [], const int Ti [],
 		     idxtype* perm, idxtype* iperm);
 
-void col_metis_order(int j0, int j1, int i2,
-		     const int Tp[], const int Ti[], int ans[]);
-
 #endif
 
