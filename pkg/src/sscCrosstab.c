d9a5ab557adfa8b1cb7dad88680213b738cff86f
bates <unknown@noreply.com>
2004-05-30 22:38:37 +0000

    Experiments with projecting
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/matrix@184 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/src/sscCrosstab.c
+++ pkg/src/sscCrosstab.c
@@ -170,7 +170,7 @@ SEXP sscCrosstab_groupedPerm(SEXP ctab)
  * 
  * @param ctab pointer to a sscCrosstab object
  * 
- * @return a pointer to an tscMatrix giving the projection of the 2,1 component
+ * @return a pointer to an sscMatrix giving the projection of the 2,1 component
  */
 SEXP sscCrosstab_project(SEXP ctab)
 {
@@ -258,6 +258,96 @@ SEXP sscCrosstab_project(SEXP ctab)
 	Free(TTi); Free(Tj); Free(AAi);
     }
     if (nf > 1 && up) {Free(Ap); Free(Ai);}
+    UNPROTECT(1);
+    return ans;
+}
+
+/** 
+ * Project the first group of columns in an sscCrosstab object onto the
+ * remaining columns.
+ * 
+ * @param ctab pointer to a sscCrosstab object
+ * 
+ * @return a pointer to an sscMatrix with the projection
+ */
+SEXP sscCrosstab_project2(SEXP ctab)
+{
+    SEXP
+	GpSlot = GET_SLOT(ctab, Matrix_GpSym),
+	iSlot = GET_SLOT(ctab, Matrix_iSym),
+	pSlot = GET_SLOT(ctab, Matrix_pSym);
+    int *Ai = INTEGER(iSlot),
+	*Ap = INTEGER(pSlot),
+	*Gp = INTEGER(GpSlot),
+	i, i2, j, j1, k, k2,
+	nf = length(GpSlot) - 1, /* number of factors */
+	nz, pos, up, *AAp, *Ti, *Cp, *ind;
+    double *Ax = REAL(GET_SLOT(ctab, Matrix_xSym));
+    SEXP ans = PROTECT(NEW_OBJECT(MAKE_CLASS("sscMatrix")));
+    
+
+    if (nf < 2) error("sscCrosstab_project2 requires more than one group");
+    up = toupper(*CHAR(STRING_ELT(GET_SLOT(ctab, Matrix_uploSym), 0))) != 'L';
+    if (up) {			/* tranpose */
+	int n = length(pSlot) - 1;	/* number of columns */
+	int nnz = length(iSlot);
+	int *ai = Calloc(nnz, int);
+	int *ap = Calloc(n + 1, int);
+	double *ax = Calloc(nnz, double);
+
+	csc_components_transpose(n, n, nnz, Ap, Ai, Ax, ap, ai, ax);
+	Ap = ap; Ai = ai; Ax = ax;
+    }
+
+    j1 = Gp[1]; i2 = Gp[nf];	/* group boundaries */
+    Cp = Calloc(j1, int);	/* first pos in col with row > i */
+    for (j = 0; j < j1; j++) Cp[j] = Ap[j] + 1;	/* Ap[j] is the diagonal */
+
+    nz = Ap[i2] - Ap[j1];	/* upper bound on nonzeros in result */
+    for (j = 0; j < j1; j++) {	
+	int nr = Ap[j + 1] - Ap[j] - 1;
+	nz += (nr * (nr - 1))/2; /* add number of pairs of rows below diag */
+    }
+
+    Ti = Calloc(nz, int);	/* temporary row indices */
+    SET_SLOT(ans, Matrix_pSym, allocVector(INTSXP, i2 - j1 + 1));
+    AAp = INTEGER(GET_SLOT(ans, Matrix_pSym)); /* column pointers */
+
+    AAp[0] = 0; pos = 0;
+    ind = Calloc(i2 - j1, int);	/* indicator of rows in same column */
+    for (i = j1; i < i2; i++) {
+	for (k = j1; k < i2; k++) ind[k - j1] = 0;
+	for (j = 0; j < j1; j++) {
+	    if (Ai[Cp[j]] == i) { /* go down the column */
+		k2 = Ap[j+1];
+		for(k = Cp[j] + 1; k < k2; k++) ind[Ai[k] - j1] = 1;
+		Cp[j]++;
+	    }
+	}
+	
+	Ti[pos++] = i - j1;	/* diagonal element */
+	for (k = i+1; k < i2; k++) { /* projected pairs */
+	    int ii = k - j1;
+	    if (ind[ii]) Ti[pos++] = ii;
+	}
+	k2 = Ap[i+1];
+	for (k = Ap[i] + 1; k < k2; k++) { /* previous off-diagonals */
+	    Ti[pos++] = Ai[k] - j1;
+	}
+	AAp[i - j1 + 1] = pos;
+    }
+    nz = AAp[i2 - j1];
+    SET_SLOT(ans, Matrix_iSym, allocVector(INTSXP, nz));
+    Memcpy(INTEGER(GET_SLOT(ans, Matrix_iSym)), Ti, nz);
+    SET_SLOT(ans, Matrix_xSym, allocVector(REALSXP, nz));
+    Ax = REAL(GET_SLOT(ans, Matrix_xSym));
+    for (j = 0; j < nz; j++) Ax[j] = 1.;
+    SET_SLOT(ans, Matrix_uploSym, ScalarString(mkChar("L")));
+    SET_SLOT(ans, Matrix_DimSym, allocVector(INTSXP, 2));
+    AAp = INTEGER(GET_SLOT(ans, Matrix_DimSym));
+    AAp[0] = AAp[1] = i2 - j1;
+    Free(Ti); Free(Cp); Free(ind);
+    if (up) {Free(Ap); Free(Ai); free(Ax);}
     UNPROTECT(1);
     return ans;
 }
--- pkg/src/sscCrosstab.h
+++ pkg/src/sscCrosstab.h
@@ -8,5 +8,6 @@ SEXP sscCrosstab(SEXP flist, SEXP upper);
 extern void ssc_metis_order(int n, const int Tp [], const int Ti [],
 			    int Perm[], int iPerm[]);
 SEXP sscCrosstab_groupedPerm(SEXP ctab);
+SEXP sscCrosstab_project2(SEXP ctab);
 
 #endif
