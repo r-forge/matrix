e25bb77e65517eac868e4b956e7a11432d45c566
bates <unknown@noreply.com>
2004-10-03 21:51:33 +0000

    Release 0.8-15
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/matrix@291 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/tripletMatrix.R
+++ pkg/R/tripletMatrix.R
@@ -18,39 +18,39 @@ setMethod("image", "tripletMatrix",
                    col.regions = grey(seq(from = 0.7, to = 0, length = 100)),
                    ...)
       {
-          if(!require("lattice", character = TRUE, quietly = TRUE))
-              stop("image methods require the lattice package")
-
           matdim <- x@Dim
-          lattice::levelplot(abs(x@x) ~ x@j * x@i,
-                             sub = sub,
-                             xlab = xlab, ylab = ylab,
-                             xlim = xlim, ylim = ylim,
-                             col.regions = col.regions,
-                             par.settings =
-                             list(background = list(col = "transparent")),
-                             panel = function(x, y, z, subscripts, at, ..., col.regions) {
-                                 x <- as.numeric(x[subscripts])
-                                 y <- as.numeric(y[subscripts])
-                                 z <- as.numeric(z[subscripts])
-                                 
-                                 numcol <- length(at) - 1
-                                 numcol.r <- length(col.regions)
-                                 col.regions <-
-                                     if (numcol.r <= numcol)
-                                         rep(col.regions, length = numcol)
-                                     else col.regions[floor(1+(1:numcol-1)*(numcol.r-1)/(numcol-1))]
-                                 zcol <- rep(NA, length(z)) #numeric(length(z))
-                                 for (i in seq(along = col.regions))
-                                     zcol[!is.na(x) & !is.na(y) & !is.na(z) & z>=at[i] & z<at[i+1]] <- i
-
-                                 ## zcol <- as.numeric(zcol[subscripts])
-                                 if (any(subscripts))
-                                     grid::grid.rect(x = x, y = y, width = 1, height = 1, 
-                                                     default.units = "native",
-                                                     gp = grid::gpar(fill = col.regions[zcol], col = NULL))
-                             }, ...)
+          levelplot(abs(x@x) ~ x@j * x@i,
+                    sub = sub,
+                    xlab = xlab, ylab = ylab,
+                    xlim = xlim, ylim = ylim,
+                    col.regions = col.regions,
+                    par.settings = list(background = list(col = "transparent")),
+                    panel = function(x, y, z, subscripts, at, ..., col.regions)
+                {
+                    x <- as.numeric(x[subscripts])
+                    y <- as.numeric(y[subscripts])
+                    
+                    numcol <- length(at) - 1
+                    numcol.r <- length(col.regions)
+                    col.regions <-
+                        if (numcol.r <= numcol)
+                            rep(col.regions, length = numcol)
+                        else col.regions[floor(1+(1:numcol-1)*(numcol.r-1)/
+                                               (numcol-1))]
+                    zcol <- rep(NA, length(z)) #numeric(length(z))
+                    for (i in seq(along = col.regions))
+                        zcol[!is.na(x) & !is.na(y) & !is.na(z) &
+                             z>=at[i] & z<at[i+1]] <- i
+                    
+                    zcol <- as.numeric(zcol[subscripts])
+                    if (any(subscripts))
+                        grid.rect(x = x, y = y, width = 1, height = 1, 
+                                  default.units = "native",
+                                  gp = gpar(fill = col.regions[zcol],
+                                  col = NULL))
+                }, ...)
       })
+
 
 
 
