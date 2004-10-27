a30d95d89dbc7a7024d33d1ccd7c1812e251d6a4
bates <unknown@noreply.com>
2004-10-27 21:35:13 +0000

    lmeRep_invert now applies to multiple, nested grouping factors
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/matrix@304 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/src/cscBlocked.c
+++ pkg/src/cscBlocked.c
@@ -7,12 +7,11 @@ SEXP cscBlocked_validate(SEXP x)
 	xp = GET_SLOT(x, Matrix_xSym),
 	dp = getAttrib(xp, R_DimSymbol);
     int *pv = INTEGER(pp),
-	*iv = INTEGER(ip),
 	*dim = INTEGER(dp),
 	ncol = length(pp) - 1;
     int nnz = pv[ncol];
 
-    if (!isReal(xp))
+    if (!(isReal(xp) && isArray(xp)))
 	return ScalarString(mkChar("slot x should be a real array"));
     if (length(dp) != 3)
 	return ScalarString(mkChar("slot x should be a 3-dimensional array"));
@@ -46,8 +45,9 @@ SEXP cscBlocked_validate(SEXP x)
  * @param b matrix to be multiplied
  * @param ldb leading dimension of b as declared in the calling
  *        routine
+ * @param beta scalar multiplier of c
  * @param c product matrix to be modified
- * @param ldc leading dimension of b as declared in the calling
+ * @param ldc leading dimension of c as declared in the calling
  *        routine
  */
 void
@@ -58,33 +58,83 @@ cscBlocked_mm(char side, char transa, int m, int n, int k,
 	      const double b[], int ldb,
 	      double beta, double c[], int ldc)
 {
-    int lside = (side == 'L' || side == 'l') ? 1 : 0;
-    int tra = (transa == 'T' || transa == 't') ? 1 : 0;
+    int j, kk, lside = (side == 'L' || side == 'l');
+    int ncb, nrb, sz = nr * nc, tra = (transa == 'T' || transa == 't');
 
-    if (lside && !tra) {	/* only case written so far */
-	int j, ncb = k/nc, nrb = m/nr, sz = nc * nr;
-
-	if (nr < 1 || nc < 1 || m < 0 || n < 0 || k < 0 ||
-	    m % nr || k % nc)
-	    error("incompatible dims m=%d, n=%d, k=%d, nr=%d, nc=%d",
+    if (nr < 1 || nc < 1 || m < 0 || n < 0 || k < 0)
+	error("improper dims m=%d, n=%d, k=%d, nr=%d, nc=%d",
 		  m, n, k, nr, nc);
-	if (ldb < k) error("incompatible dims k=%d, ldb=%d", k, ldb);
-	if (ldc < n) error("incompatible dims n=%d, ldc=%d", n, ldc);
+    if (ldc < n) error("incompatible dims n=%d, ldc=%d", n, ldc);
+    if (lside) {
+	if (ldb < k)
+	    error("incompatible L dims k=%d, ldb=%d, n=%d, nr=%d, nc=%d",
+		  k, ldb, n, nr, nc);
+	if (tra) {
+	    if (m % nc || k % nr)
+		error("incompatible LT dims m=%d, k = %d, nr=%d, nc=%d",
+		      m, k, nr, nc);
+	    nrb = k/nr; ncb = m/nc;
+	} else {
+	    if (m % nr || k % nc)
+		error("incompatible LN dims m=%d, k = %d, nr=%d, nc=%d",
+		      m, k, nr, nc);
+	    nrb = m/nr; ncb = k/nc;
+	}
 	for (j = 0; j < ncb; j++) {
-	    int i, i2 = ap[j+1];
-	    for (i = ap[j]; i < i2; i++) {
-		int rb = ai[i];
-		if (rb < 0 || rb >= nrb)
-		    error("incompatible dims m=%d, nr=%d, rb=%d",
-			  m, nr, rb);
-		F77_CALL(dgemm)("N", "N", &nr, &n, &nc,
-				&alpha, ax + i * sz, &nr,
-				b + j * nc, &ldb,
-				&beta, c + rb * nr, &ldc);
+	    int j2 = ap[j + 1];
+	    for (kk = ap[j]; kk < j2; kk++) {
+		int ii = ai[kk];
+		if (ii < 0 || ii >= nrb)
+		    error("improper row index ii=%d, nrb=%d", ii, nrb);
+		if (tra) {
+		    F77_CALL(dgemm)("T", "N", &nc, &n, &nr,
+				    &alpha, ax + j*sz, &nr,
+				    b + ii * nr, &ldb,
+				    &beta, c + j * nc, &ldc);
+		} else {
+		    F77_CALL(dgemm)("N", "N", &nr, &n, &nc,
+				    &alpha, ax + j * sz, &nr,
+				    b + j*nc, &ldb,
+				    &beta, c + ii * nr, &ldc);
+		}
 	    }
 	}
-	    
     } else {
-	error("Call to cscBlocked_mm must have side == 'L' and transa == 'N'");
+	error("Call to cscBlocked_mm must have side == 'L'");
+    }
+}
+
+/** 
+ * Invert a triangular sparse blocked matrix.  This is not done in
+ * place because the number of non-zero blocks in A-inverse can be
+ * different than the number of non-zero blocks in A.
+ * 
+ * @param upper 'U' indicates upper triangular, 'L' lower
+ * @param unit 'U' indicates unit diagonal, 'N' non-unit
+ * @param n number of columns of blocks in A and A-inverse
+ * @param nr number of rows per block in A and A-inverse
+ * @param nc number of columns per block in A and A-inverse
+ * @param ap vector of column pointers for A
+ * @param ai vector of row indices for A
+ * @param ax contents of the non-zero blocks of A
+ * @param aip vector of column pointers for A-inverse
+ * @param aii vector of row indices for A-inverse
+ * @param aix contents of the non-zero blocks of A-inverse
+ */
+void
+cscBlocked_tri(char upper, char unit, int n, int nr, int nc,
+	       const int ap[], const int ai[], const double ax[],
+	       int aip[], int aii[], double aix[])
+{
+    int iup = (upper == 'U' || upper == 'u') ? 1 : 0,
+	iunit = (unit == 'U' || unit == 'u') ? 1 : 0;
+    
+    if (!iunit)
+	error("Code for non-unit triangular matrices not yet written");
+    if (ap[n] > 0)
+	error("Code for non-trivial unit inverse not yet written");
+    else {
+	if (aip[n] == 0) return;
+	error ("Structure of A and A-inverse does not agree");
     }
 }
--- pkg/src/cscBlocked.h
+++ pkg/src/cscBlocked.h
@@ -2,6 +2,7 @@
 #define MATRIX_CSCBLOCKED_H
 
 #include "Mutils.h"
+#include <R_ext/Lapack.h>
 
 SEXP cscBlocked_validate(SEXP x);
 void cscBlocked_mm(char side, char transa, int m, int n, int k,
@@ -10,5 +11,8 @@ void cscBlocked_mm(char side, char transa, int m, int n, int k,
 		   const double ax[],
 		   const double b[], int ldb,
 		   double beta, double c[], int ldc);
+void cscBlocked_tri(char upper, char unit, int n, int nr, int nc,
+		    const int ap[], const int ai[], const double ax[],
+		    int aip[], int aii[], double aix[]);
 
 #endif
