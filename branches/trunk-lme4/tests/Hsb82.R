00b4886625a3a52bb2741277657989d89aa981a7
bates <unknown@noreply.com>
2004-11-02 21:30:03 +0000

    add lme1 calls; update results
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/matrix@318 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- branches/trunk-lme4/tests/Hsb82.R
+++ branches/trunk-lme4/tests/Hsb82.R
@@ -1,6 +1,6 @@
 library(lme4)
 lme(mAch ~ meanses*cses + sector*cses,
     data = Hsb82, random = ~ cses|school)
-lme1(mAch ~ meanses*cses + sector*cses,
-    data = Hsb82, random = ~ cses|school)
+show(lme1(mAch ~ meanses*cses + sector*cses,
+          data = Hsb82, random = ~ cses|school))
 q("no")
