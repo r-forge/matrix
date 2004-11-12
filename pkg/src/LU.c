d7bef6ece7d99e420c40b151e429e15c3e97d56f
bates <unknown@noreply.com>
2004-11-12 21:04:36 +0000

    initialize all slots in a NEW_OBJECT
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/matrix@332 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/src/LU.c
+++ pkg/src/LU.c
@@ -1,5 +1,6 @@
 #include "LU.h"
 
+/* FIXME - add the permutation matrix P to the result */
 SEXP LU_expand(SEXP x)
 {
     SEXP L = PROTECT(NEW_OBJECT(MAKE_CLASS("trMatrix"))),
@@ -8,7 +9,10 @@ SEXP LU_expand(SEXP x)
 	nms = PROTECT(allocVector(STRSXP, 2)),
 	lux = GET_SLOT(x, Matrix_xSym),
 	dd = GET_SLOT(x, Matrix_DimSym);
+    int i, n;
+    double *lx;
 
+    n = *INTEGER(dd);
     SET_STRING_ELT(nms, 0, mkChar("L"));
     SET_STRING_ELT(nms, 1, mkChar("U"));
     setAttrib(val, R_NamesSymbol, nms);
@@ -18,9 +22,15 @@ SEXP LU_expand(SEXP x)
     SET_SLOT(L, Matrix_DimSym, dd);
     SET_SLOT(L, Matrix_uploSym, ScalarString(mkChar("L")));
     SET_SLOT(L, Matrix_diagSym, ScalarString(mkChar("U")));
+    SET_SLOT(L, Matrix_rcondSym, allocVector(REALSXP, 0));
+    SET_SLOT(L, Matrix_factorization, allocVector(VECSXP, 0));
     make_array_triangular(REAL(GET_SLOT(L, Matrix_xSym)), L);
     SET_SLOT(U, Matrix_xSym, duplicate(lux));
     SET_SLOT(U, Matrix_DimSym, dd);
+    SET_SLOT(U, Matrix_uploSym, ScalarString(mkChar("U")));
+    SET_SLOT(U, Matrix_diagSym, ScalarString(mkChar("N")));
+    SET_SLOT(U, Matrix_rcondSym, allocVector(REALSXP, 0));
+    SET_SLOT(U, Matrix_factorization, allocVector(VECSXP, 0));
     make_array_triangular(REAL(GET_SLOT(U, Matrix_xSym)), U);
     UNPROTECT(4);
     return val;
