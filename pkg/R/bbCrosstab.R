aed15248f9d151d986481340d5308a5a5537af36
bates <unknown@noreply.com>
2004-11-23 15:38:31 +0000

    add blocked crosstabulations
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/matrix@348 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/bbCrosstab.R
+++ pkg/R/bbCrosstab.R
@@ -1,18 +1,23 @@
-bbCrosstab <- function(flist) {
+bCrosstab <- function(flist) {
     ind <- function(i,j) ((i-1) * i)/2 + j # index in rowwise lower triangle
-    flist <- lapply(as.list(flist), function (x) as(x, "factor")[drop = TRUE])
+    ## Coerce flist to a list of factors with no unused levels
+    flist <- lapply(as.list(flist), function (x) as.factor(x)[drop = TRUE])
+
     nfac <- length(flist)
-    if (nfac < 1) return(new("bbSparseSy", x = list(), uplo = 'L'))
+    if (nfac < 1) error("flist must be a non-empty list of factors")
+                                        # Check for consistent lengths
+    nobs <- length(flist[[1]])
+    if (any(lapply(flist, length) != nobs))
+        stop("all factors in flist must have the same length")
+    ones <- rep(1, nobs)
+    ## Order the factors by non-increasing length(levels(el))
     nlev <- unlist(lapply(flist, function(x) length(levels(x))))
     ord <- rev(order(nlev))
     if (any(ord != seq(along = ord))) {
         nlev <- nlev[ord]
         flist <- flist[ord]
     }
-    nobs <- length(flist[[1]])
-    if (any(lapply(flist, length) != nobs))
-        stop("all factors in flist must have the same length")
-    ones <- rep(1, nobs)
+                                        # establish output list and names
     lst <- vector("list", choose(nfac + 1, 2))
     if (is.null(nms <- names(flist))) nms <- paste("V", 1:nfac, sep = "")
     nmat <- outer(nms, nms, FUN = "paste", sep = ":")
@@ -20,64 +25,63 @@ bbCrosstab <- function(flist) {
     zb <- lapply(flist, function(x) as.integer(x) - 1:1) # zero-based indices
     for (j in 1:nfac) {
         for (i in j:nfac) {
-            lst[[ind(i,j)]] <- as(as(new("tripletMatrix",
-                                       i = zb[[i]], j = zb[[j]],
-                                       x = ones, Dim = nlev[c(i,j)]),
-                                   "cscMatrix"),
-                                "cscBlocked")
+            IND <- ind(i,j)
+            lst[[IND]] <- as(new("tripletMatrix",
+                                 i = zb[[i]], j = zb[[j]],
+                                 x = ones, Dim = nlev[c(i,j)]),
+                             "cscMatrix")
             nms[ind(i,j)] <- nmat[i, j]
         }
     }
     names(lst) <- nms
-    new("bbCrosstab", x = lst, uplo = "L")
+    list(flist = flist, ctab = lst)
 }
 
-setMethod("isNested", signature(object = "bbCrosstab"),
-          function(object, ...)
-          all(unlist(lapply(object@x, function(x) any(diff(x@p) > 1)))))
-
-bbCrossNfac <- function(x) {            # determine the number of factors
-    x <- as(x, "bbCrosstab")
-    as.integer((-1 + sqrt(1 + 8*length(x@x)))/2)
-}
-
-bbCrossProject <- function(x) {         # Project the first column onto the others
+ctab2bblist <- function(ctab, nf, nc)
+{
     ind <- function(i,j) ((i-1) * i)/2 + j # index in compressed lower triangle
-    x <- as(x, "bbCrosstab")
-    nf <- bbCrossNfac(x)
-    if (nf < 2) stop("Number of factors must be > 1 to project")
-    ans <- new("bbCrosstab", x = x@x[-(1:nf)], uplo = "L")
-    for (i in 2:nf) {
-        ans@x[[ind(i-1,i-1)]] <-
-            as(tcrossprod(as(x@x[[i]], "cscMatrix")), "cscBlocked") 
+    ans <- vector("list", length(ctab))
+    names(ans) <- names(ctab)
+    for (j in 1:nf) {
+        for (i in j:nf) {
+            IND <- ind(i, j)
+            ctij <- ctab[[IND]]
+            ans[[IND]] <- new("cscBlocked", p = ctij@p, i = ctij@p,
+                              x = array(0, dim = c(nc[i], nc[j], length(ctij@x))))
+        }
     }
     ans
 }
 
-bbCrossPerms <- function(x) {
-    ind <- function(i,j) ((i-1) * i)/2 + j
-    needsPerm <- function(x) any(diff(as(x, "cscBlocked")@p) != 1)
-    x <- as(x, "bbCrosstab")
-    nf <- bbCrossNfac(x)
-    perm <- vector("list", nf)
-#    Lmat <- new("bbSparseTr", uplo = 'L', diag = 'U',
-#                Linv = vector("list", nf), x = x@x)
-    Linv <- vector("list", nf)
-    for (j in 1:nf) {
-        db <- x@x[[ind(j,j)]]             # diagonal block
-        if (needsPerm(db)) {
-            res <- .Call("sscMatrix_ldl_symbolic",
-                         as(as(db, "cscMatrix"), "sscMatrix"), TRUE)
-        } else {
-            ncol <- length(db@p) - 1
-            res <- list(rep(-(1:1), ncol), NULL, seq(ncol) - 1:1)
-        }
-        perm[[j]] <- res[[3]]
-        Linv[[j]] <- as(.Call("Parent_inverse", res[[1]], TRUE),
-                        "cscBlocked")    
+Lstruct <- function(bcr, nc = rep(1, nf)) {
+    ind <- function(i,j) ((i-1) * i)/2 + j # index in compressed lower triangle
+    ctab <- bcr$ctab
+    fl <- bcr$flist
+    nf <- length(fl)
+    Linv <- vector("list", length(fl))
+    names(Linv) <- names(fl)
+                                        
+    if (all(unlist(lapply(ctab, function(x) all(diff(x@p)== 1))))) { # nested
+        ZtZ <- ctab2bblist(ctab, nf, nc)
+        for (j in 1:nf) Linv[[j]] <- ZtZ[[ind(j, j)]]
+        return(list(flist = fl, ZtZ = ZtZ, Lmat = ZtZ, Linv = Linv))
     }
-#    Lmat$Linv <- Linv
-#    list(perm = perm, Lmat = Lmat)
-    list(perm = perm, Linv = Linv) 
+    ## non-nested case - here things get interesting
+    ct11 <- ctab[[1]]
+    Linv[[1]] <- new("cscBlocked", p = ct11@p, i = ct11@i,
+                     x = array(0, dim = c(nc[1], nc[1], length(ct11@x))))
+    for (i in 1:(nf - 1)) {
+        ip1 <- i + 1
+        res <- .Call("bCrosstab_project", ctab, i)
+        fac <- fl[[ip1]]
+        fl[[ip1]] <- factor(as.character(fac), levels = levels(fac)[1+res$perm])
+        ctab <- .Call("bCrosstab_permute", res$ctab, i, res$perm)
+        rLi <- res$Linv
+        Linv[[ip1]] <- new("cscBlocked", p = rLi@p, i = rLi@i,
+                           x = array(0, dim = c(nc[ip1], nc[ip1], length(rLi@x))))
+    }
+    list(flist = fl, ZtZ = ctab2bblist(bCrosstab(fl)$ctab, nf, nc),
+         Lmat = ctab2bblist(ctab, nf, nc), Linv = Linv)
+    ## FIXME: You probably don't want to define Lmat from ctab because
+    ## it has off-diagonal elements generated from the inverses of the diagonals
 }
-
