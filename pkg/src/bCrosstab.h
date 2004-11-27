d6a6581f5832054efd4ff01f3b184fb762c9b68b
bates <unknown@noreply.com>
2004-11-27 01:45:39 +0000

    Add block updates in bCrosstab_convert
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/matrix@356 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/src/bCrosstab.h
+++ pkg/src/bCrosstab.h
@@ -4,8 +4,12 @@
 #include "Mutils.h"
 #include "cscMatrix.h"
 #include "Metis_utils.h"
+#include "triplet_to_col.h"
+#include "ldl.h"
+#include "sscMatrix.h"
+#include "tscMatrix.h"
 
 SEXP bCrosstab_project(SEXP ctab, SEXP j);
-SEXP bCrosstab_permute(SEXP ctab, SEXP i, SEXP perm);
+SEXP bCrosstab_convert(SEXP bCtab);
 
 #endif
