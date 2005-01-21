4ec50a9e1de0ae405bf97dc99b7707c529d27cba
bates <unknown@noreply.com>
2005-01-21 22:01:21 +0000

    Release 0.6-12
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/matrix@449 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- branches/trunk-lme4/tests/Pixel.R
+++ branches/trunk-lme4/tests/Pixel.R
@@ -1,4 +1,4 @@
 library(lme4)
 lme(pixel ~ day + I(day^2), Pixel, list(Dog = ~ day, DS = ~ 1))
-show(lme1(pixel ~ day + I(day^2), Pixel, list(Dog = ~ day, DS = ~ 1)))
+lmer(pixel ~ day + I(day^2) + (1|DS) + (day|Dog), Pixel)
 q("no")
