e7cd87f55cfa3cf80d3b3849931e1c956583b0a4
maechler <unknown@noreply.com>
2005-01-31 21:14:46 +0000

    use *Rout as well
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/matrix@470 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/tests/poMatrix.R
+++ pkg/tests/poMatrix.R
@@ -1,9 +1,14 @@
 library(Matrix)
-h9 = Hilbert(9)
+h9 <- Hilbert(9)
 str(h9)
-f9 = as(chol(h9), "trMatrix")
-str(h9)
+f9 <- as(chol(h9), "trMatrix")
+str(h9)# now has factorization
+if(is.null(h9@factorization))
+  stop("didn't auto-update factorization slot")
 str(f9)
 rcond(h9)
 rcond(f9)
-all.equal(h9, crossprod(f9))
+options(digits=4)
+crossprod(f9)# looks the same as
+h9 # but not internally!
+##   i.e. this is all wrong : all.equal(h9, crossprod(f9))
