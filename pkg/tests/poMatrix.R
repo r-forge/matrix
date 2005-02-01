8082fe9eed3c224f8ecec66494751f3d55a9524b
maechler <unknown@noreply.com>
2005-02-01 14:41:09 +0000

    more po-tests
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/matrix@473 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/tests/poMatrix.R
+++ pkg/tests/poMatrix.R
@@ -1,13 +1,19 @@
+### Testing positive definite matrices
+
 library(Matrix)
+
+stopifnot(c(0,0) == dim(Hilbert(0)))
+
 h9 <- Hilbert(9)
 str(h9)
-f9 <- as(chol(h9), "trMatrix")
-str(h9)# now has factorization
-if(is.null(h9@factorization))
-  stop("didn't auto-update factorization slot")
-str(f9)
+all.equal(determinant(h9)$modulus, -96.7369450737858, tol= 1e-15)
+stopifnot(0 == length(h9@factorization))# nothing yet
+str(f9 <- as(chol(h9), "trMatrix"))
+## h9 now has factorization
+stopifnot(names(h9@factorization) == "Cholesky")
 rcond(h9)
 rcond(f9)
+str(h9)# has 'rcond' and 'factorization'
 options(digits=4)
 crossprod(f9)# looks the same as
 h9 # but not internally!
