49f5ef8f957ef18986b5e8d7256cac49120afc26
bates <unknown@noreply.com>
2005-02-02 19:40:26 +0000

    renaming and rearranging classes
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/matrix@491 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/tests/dpoMatrix.R
+++ pkg/tests/dpoMatrix.R
@@ -8,12 +8,12 @@ h9 <- Hilbert(9)
 str(h9)
 all.equal(determinant(h9)$modulus, -96.7369450737858, tol= 1e-15)
 stopifnot(0 == length(h9@factors))# nothing yet
-str(f9 <- as(chol(h9), "trMatrix"))
+str(f9 <- as(chol(h9), "dtrMatrix"))
 ## h9 now has factorization
 stopifnot(names(h9@factors) == "Cholesky")
 rcond(h9)
 rcond(f9)
-str(h9)# has 'rcond' and 'factorization'
+str(h9)# has 'rcond' and 'factors'
 options(digits=4)
 crossprod(f9)# looks the same as
 h9 # but not internally!
