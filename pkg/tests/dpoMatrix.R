4a5bd138c3db2b54c2ab6506425585f23d498364
maechler <unknown@noreply.com>
2005-02-04 23:34:01 +0000

    some new tests, using stopifnot()
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/matrix@509 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/tests/dpoMatrix.R
+++ pkg/tests/dpoMatrix.R
@@ -11,10 +11,11 @@ stopifnot(0 == length(h9@factors))# nothing yet
 str(f9 <- as(chol(h9), "dtrMatrix"))
 ## h9 now has factorization
 stopifnot(names(h9@factors) == "Cholesky")
-rcond(h9)
-rcond(f9)
+stopifnot(all.equal(rcond(h9), 9.0938e-13))
+stopifnot(all.equal(rcond(f9), 9.1272e-7, tol = 1e-6))# more precision fails
 str(h9)# has 'rcond' and 'factors'
 options(digits=4)
-crossprod(f9)# looks the same as
-h9 # but not internally!
-##   i.e. this is all wrong : all.equal(h9, crossprod(f9))
+(cf9 <- crossprod(f9))# looks the same as  h9 :
+stopifnot(all.equal(as(h9, "matrix"),
+                    as(cf9,"matrix"), tol= 1e-15))
+
