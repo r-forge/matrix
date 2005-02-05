09ac0874a2e7dd38d25689005c554517ce709573
maechler <unknown@noreply.com>
2005-02-05 14:41:29 +0000

    add "surprising formula"
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/matrix@520 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/tests/matr-exp.R
+++ pkg/tests/matr-exp.R
@@ -13,6 +13,9 @@ e2 <- expm(m2)
 assert.EQ.mat(e2, rbind(c(-0.73575875814475, 0.55181909965810),
 			c(-1.47151759908826, 1.10363824071557)),
 	      tol = 1e-14)
+## The ``surprising identity''      det(exp(A)) == exp( tr(A) )
+## or                           log det(exp(A)) == tr(A) :
+stopifnot(all.equal(determinant(e2)$modulus, sum(diag(m2))))
 
 m3 <- Matrix(cbind(0,rbind(6*diag(3),0)), nc = 4)
 e3 <- expm(m3)
