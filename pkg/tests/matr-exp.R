d03ac61ceaf72dc346df601a02d2af81927cd4cf
maechler <unknown@noreply.com>
2005-02-07 09:30:58 +0000

    tol was too tight
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/matrix@524 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/tests/matr-exp.R
+++ pkg/tests/matr-exp.R
@@ -14,7 +14,7 @@ m2 <- Matrix(c(-49, -64, 24, 31), nc = 2)
 e2 <- expm(m2)
 assert.EQ.mat(e2, rbind(c(-0.73575875814475, 0.55181909965810),
 			c(-1.47151759908826, 1.10363824071557)),
-	      tol = 1e-14)
+	      tol = 1e-13)
 ## The ``surprising identity''      det(exp(A)) == exp( tr(A) )
 ## or                           log det(exp(A)) == tr(A) :
 stopifnot(all.equal(determinant(e2)$modulus, sum(diag(m2))))
