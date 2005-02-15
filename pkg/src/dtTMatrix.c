eaec2f396a63116cc423756ba34e2093022b258e
bates <unknown@noreply.com>
2005-02-15 22:28:03 +0000

    triangular, triplet sparse matrices
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/matrix@551 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/src/dtTMatrix.c
@@ -0,0 +1,27 @@
+				/* Sparse triangular matrices in triplet format */
+#include "dtTMatrix.h"
+
+SEXP dtTMatrix_validate(SEXP x)
+{
+    return ScalarLogical(1);
+}
+
+SEXP dtTMatrix_as_dtrMatrix(SEXP x)
+{
+    SEXP val = PROTECT(NEW_OBJECT(MAKE_CLASS("dtrMatrix"))),
+	DimP = GET_SLOT(x, Matrix_DimSym),
+	xiP = GET_SLOT(x, Matrix_iSym);
+    int k, m = INTEGER(DimP)[0], n = INTEGER(DimP)[1], nnz = length(xiP);
+    int *xi = INTEGER(xiP), *xj = INTEGER(GET_SLOT(x, Matrix_jSym)),
+	sz = m * n;
+    double *tx = REAL(ALLOC_SLOT(val, Matrix_xSym, REALSXP, sz)),
+	*xx = REAL(GET_SLOT(x, Matrix_xSym));
+    
+    SET_SLOT(val, Matrix_DimSym, duplicate(DimP));
+    SET_SLOT(val, Matrix_uploSym, duplicate(GET_SLOT(x, Matrix_uploSym)));
+    SET_SLOT(val, Matrix_diagSym, duplicate(GET_SLOT(x, Matrix_diagSym)));
+    AZERO(tx, sz);
+    for (k = 0; k < nnz; k++) tx[xi[k] + xj[k] * m] = xx[k];
+    UNPROTECT(1);
+    return val;
+}
