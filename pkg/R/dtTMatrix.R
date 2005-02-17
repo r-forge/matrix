bc4befac6be2948ce25aa14c038c876c8cb37849
bates <unknown@noreply.com>
2005-02-17 01:18:02 +0000

    Coercions and methods for the dsTMatrix class
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/matrix@557 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/dtTMatrix.R
+++ pkg/R/dtTMatrix.R
@@ -1,9 +1,15 @@
+  ### Coercion and Methods for Triangular Triplet Matrices
+
+## Conversion to dense storage is first to a dtrMatrix
 setAs("dtTMatrix", "dtrMatrix",
       function(from) .Call("dtTMatrix_as_dtrMatrix", from))
 
 setAs("dtTMatrix", "matrix",
       function(from) as(as(from, "dtrMatrix"), "matrix"))
-      
+
+setAs("dtTMatrix", "dgeMatrix",
+      function(from) as(as(from, "dtrMatrix"), "dgeMatrix"))
+
 setMethod("t", signature(x = "dtTMatrix"),
           function(x)
           new("dtTMatrix", Dim = rev(x@Dim), diag = x@diag,
