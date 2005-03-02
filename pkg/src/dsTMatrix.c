c66fc47434c939408cafb22695a7534c2af19081
bates <unknown@noreply.com>
2005-03-02 19:53:53 +0000

    more coercions
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/matrix@589 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/src/dsTMatrix.c
+++ pkg/src/dsTMatrix.c
@@ -9,7 +9,7 @@ SEXP dsTMatrix_validate(SEXP x)
     int *dims = INTEGER(GET_SLOT(x, Matrix_DimSym));
 
     if (dims[0] != dims[1])
-	return mkString(_("dsTMatrix must have ncol == nrow"));
+	return mkString(_("dsTMatrix must be square"));
     if (length(xiP) != length(xjP) || length(xjP) != length(xxP))
 	return mkString(_("slots i, j and x must have the same length"));
     return ScalarLogical(1);
