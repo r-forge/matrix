0212fe00151639416aed494ae2df5c502acbc96c
maechler <unknown@noreply.com>
2005-03-04 17:35:12 +0000

    update & extend tests
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/matrix@612 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/tests/dgCMatrix.R
+++ pkg/tests/dgCMatrix.R
@@ -1,11 +1,11 @@
 library(Matrix)
 
 data(mm)
-is(mm)
-stopifnot(dim(mm) == c(1850, 712))
-dimnames(mm) # empty  {but currently "NULL" instead of list(NULL,NULL)}
+stopifnot(is(mm) == c("dgCMatrix", "dMatrix", "Matrix"),
+          dim(mm) == c(1850, 712),
+          identical(dimnames(mm), list(NULL,NULL)))
 str(mm)
 tmm <- t(mm)
 str(tmm)
-validObject(tmm)
-stopifnot(all.equal(as(tmm, "matrix"), t(as(mm, "matrix"))))
+stopifnot(validObject(tmm),
+          identical(as(tmm, "matrix"), t(as(mm, "matrix"))))
--- pkg/tests/dtpMatrix.R
+++ pkg/tests/dtpMatrix.R
@@ -1,18 +1,29 @@
+### triangular packed
 library(Matrix)
-tp6 <- as(chol(Hilbert(6)),"dtpMatrix")
-validObject(tp6)
+## round(): "Math2" group method
+round(tp6 <- as(chol(Hilbert(6)),"dtpMatrix"), 3)
+1/tp6 # "Arith" group : gives 'dgeMatrix'
 str(tp6)
-show(tp6)
-all.equal(tp6 %*% diag(6), as(tp6, "matrix"))
-t(tp6)
-determinant(tp6)
-rcond(tp6)
+stopifnot(validObject(tp6),
+          all.equal(tp6 %*% diag(6), as(tp6, "matrix")),
+          class((tt6 <- t(tp6))) == "dtpMatrix",
+          tp6@uplo == "U" && tt6@uplo == "L")
+if(FALSE) # FIXME : still fails
+diag(6) %*% tp6
+(tr6 <- as(tp6, "dtrMatrix")) ## FIXME: prints horribly
+D. <- determinant(tp6)
+rc <- rcond(tp6)
+stopifnot(all.equal(D.$modulus, -6.579251212),
+          all.equal(rc, 1.791511257e-4),
+          rc == tp6@rcond)
 norm(tp6, "I")
 norm(tp6, "1")
 norm(tp6, "F")
 object.size(tp6)
 object.size(as(tp6, "dtrMatrix"))
 object.size(as(tp6, "matrix"))
+
+## larger case
 set.seed(123)
 rl <- new("dtpMatrix", uplo="L", diag="N", Dim = rep.int(1000:1000,2),
           x = rnorm(500*1001))
@@ -21,7 +32,7 @@ str(rl)
 norm(rl, "I")
 norm(rl, "1")
 norm(rl, "F")
-rcond(rl)
+rcond(rl)# 0 !
 all.equal(rl %*% diag(1000), as(rl, "matrix"))
 object.size(rl)
 object.size(as(rl, "dtrMatrix"))
