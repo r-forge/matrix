53c17d53e5d3a673844b5d1ebb4741b7d80a4724
maechler <unknown@noreply.com>
2005-03-05 21:21:31 +0000

    comments and cosmetic, setAs(.) first
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/matrix@619 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/dgCMatrix.R
+++ pkg/R/dgCMatrix.R
@@ -1,39 +1,15 @@
-setMethod("crossprod", signature(x = "dgCMatrix", y = "missing"),
-          function(x, y = NULL)
-          .Call("csc_crossprod", x))
+#### Sparse Matrices in Compressed column-oriented format
 
-setMethod("crossprod", signature(x = "dgCMatrix", y = "matrix"),
-          function(x, y = NULL)
-          .Call("csc_matrix_crossprod", x, y))
-
-setMethod("crossprod", signature(x = "dgCMatrix", y = "numeric"),
-          function(x, y = NULL)
-          .Call("csc_matrix_crossprod", x, as.matrix(y)))
-
-setMethod("tcrossprod", signature(x = "dgCMatrix"),
-          function(x)
-          .Call("csc_tcrossprod", x))
-
-setMethod("diag", signature(x = "dgCMatrix"),
-          function(x = 1, nrow, ncol = n)
-          .Call("csc_getDiag", x))
-
-## try to define for "Matrix" -- once and for all -- but that fails -- why?
-setMethod("dim", signature(x = "dgCMatrix"),
-          function(x) x@Dim, valueClass = "integer")
+### contains = "dMatrix"
 
 setAs("dgCMatrix", "dgTMatrix",
-      function(from)
-      .Call("csc_to_dgTMatrix", from)
-      )
+      function(from) .Call("csc_to_dgTMatrix", from))
 
 setAs("dgCMatrix", "matrix",
-      function(from)
-      .Call("csc_to_matrix", from))
+      function(from) .Call("csc_to_matrix", from))
 
 setAs("dgCMatrix", "dgeMatrix",
-      function(from)
-      .Call("csc_to_dgeMatrix", from))
+      function(from) .Call("csc_to_dgeMatrix", from))
 
 setAs("dgCMatrix", "dgBCMatrix",
       function(from) new("dgBCMatrix", p = from@p, i = from@i,
@@ -45,6 +21,25 @@ setAs("matrix", "dgCMatrix",
           .Call("matrix_to_csc", from)
       })
 
+setMethod("crossprod", signature(x = "dgCMatrix", y = "missing"),
+          function(x, y = NULL) .Call("csc_crossprod", x))
+
+setMethod("crossprod", signature(x = "dgCMatrix", y = "matrix"),
+          function(x, y = NULL) .Call("csc_matrix_crossprod", x, y))
+
+setMethod("crossprod", signature(x = "dgCMatrix", y = "numeric"),
+          function(x, y = NULL) .Call("csc_matrix_crossprod", x, as.matrix(y)))
+
+setMethod("tcrossprod", signature(x = "dgCMatrix"),
+          function(x) .Call("csc_tcrossprod", x))
+
+setMethod("diag", signature(x = "dgCMatrix"),
+          function(x = 1, nrow, ncol = n) .Call("csc_getDiag", x))
+
+## try to define for "Matrix" -- once and for all -- but that fails -- why?
+setMethod("dim", signature(x = "dgCMatrix"),
+          function(x) x@Dim, valueClass = "integer")
+
 setMethod("t", signature(x = "dgCMatrix"),
           function(x) .Call("csc_transpose", x),
           valueClass = "dgCMatrix")
@@ -54,4 +49,3 @@ setMethod("image", "dgCMatrix",
               x = as(x, "dgTMatrix")
               callGeneric()
           })
-
