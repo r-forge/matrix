4a372b413c7ebc363c23a626c647654494235da2
maechler <unknown@noreply.com>
2005-03-13 21:01:45 +0000

    updates
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/matrix@634 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/tests/validObj.R
+++ pkg/tests/validObj.R
@@ -60,9 +60,9 @@ try( chk.matrix(as(cm, "Matrix")) )# gives an error: "Matrix" has NULL 'dim()'
 
 ## Cholesky
 chk.matrix(ch <- chol(cm))
-## FIXME:
-try( chk.matrix(ch2 <- chol(as(cm, "dsyMatrix"))) ) # should not give an error
-try( chk.matrix(ch3 <- chol(as(cm, "dgeMatrix"))) ) # nor that one
+chk.matrix(ch2 <- chol(as(cm, "dsyMatrix")))
+chk.matrix(ch3 <- chol(as(cm, "dgeMatrix")))
+stopifnot(all.equal(as(ch, "matrix"), as(ch3, "matrix")))
 
 ### Very basic  triangular matrix stuff
 
