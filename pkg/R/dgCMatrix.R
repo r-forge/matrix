77fb5f8be5b2fedfa21cdb10ada939a0d3f30ae5
bates <unknown@noreply.com>
2005-06-06 15:48:13 +0000

    Fix matrix multiplication methods
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/matrix@760 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/dgCMatrix.R
+++ pkg/R/dgCMatrix.R
@@ -69,6 +69,17 @@ setMethod("image", "dgCMatrix",
           })
 
 setMethod("%*%", signature(x = "dgCMatrix", y = "dgeMatrix"),
-          function(x, y) .Call("dgCMatrix_matrix_mm", x, y, TRUE, FALSE),
+          function(x, y) .Call("csc_matrix_mm", x, y, TRUE, FALSE),
           valueClass = "dgeMatrix")
 
+setMethod("%*%", signature(x = "dgCMatrix", y = "matrix"),
+          function(x, y) .Call("csc_matrix_mm", x, y, FALSE, FALSE),
+          valueClass = "dgeMatrix")
+
+setMethod("%*%", signature(x = "dgeMatrix", y = "dgCMatrix"),
+          function(x, y) .Call("csc_matrix_mm", y, x, TRUE, TRUE),
+          valueClass = "dgeMatrix")
+
+setMethod("%*%", signature(x = "matrix", y = "dgCMatrix"),
+          function(x, y) .Call("csc_matrix_mm", y, x, FALSE, TRUE),
+          valueClass = "dgeMatrix")
