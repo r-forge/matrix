2001cd84786428f987755f7b969346b9aa53704b
bates <unknown@noreply.com>
2005-07-04 14:54:13 +0000

    Added tests for generalized mixed models
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/matrix@793 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/tests/lmer.R
+++ pkg/tests/lmer.R
@@ -22,3 +22,47 @@ options(show.signif.stars = FALSE)
 (fm4 <- lmer(decrease ~ treatment + (1|rowpos) + (1|colpos),
              OrchardSprays, poisson(), method = "Laplace"))
 
+## Adaptive Gauss-Hermite quadrature
+(fm5 <- lmer(decrease ~ treatment + (1|rowpos) + (1|colpos),
+             OrchardSprays, poisson(), method = "Laplace"))
+
+if (isTRUE(try(data(Contraception, package = 'mlmRev')) ==
+           'Contraception')) {
+    (fm1 <- lmer(use ~ urban + age + livch + (1 | district),
+                 Contraception, binomial))
+    system.time(fm1 <- lmer(use ~ urban + age + livch + (1 | district),
+                            Contraception, binomial), gc = TRUE)
+    (fm2 <- lmer(use ~ urban + age + livch + (1 | district),
+                 Contraception, binomial, method = 'Laplace'))
+    system.time(fm2 <- lmer(use ~ urban + age + livch + (1 | district),
+                            Contraception, binomial, method = 'Laplace'),
+                gc = TRUE)
+    (fm2a <- lmer(use ~ urban + age + livch + (1 | district),
+                  Contraception, binomial, method = 'AGQ'))
+    system.time(fm2a <- lmer(use ~ urban + age + livch + (1 | district),
+                             Contraception, binomial, method = 'AGQ'),
+                gc = TRUE)
+    (fm3 <- lmer(use ~ urban + age + livch + (urban | district),
+                 Contraception, binomial))
+    (fm4 <- lmer(use ~ urban + age + livch + (urban | district),
+                 Contraception, binomial, method = 'Laplace'))
+}
+if (require('MASS', quietly = TRUE)) {
+    bacteria$wk2 <- bacteria$week > 2
+    contrasts(bacteria$trt) <-
+        structure(contr.sdif(3),
+                  dimnames = list(NULL, c("diag", "encourage")))
+    (fm5 <- lmer(y ~ trt + wk2 + (1|ID), bacteria, binomial))
+    system.time(fm5 <- lmer(y ~ trt + wk2 + (1|ID), bacteria, binomial),
+                gc = TRUE)
+    (fm6 <- lmer(y ~ trt + wk2 + (1|ID), bacteria, binomial,
+                 method = 'Laplace'))
+    system.time(fm6 <- lmer(y ~ trt + wk2 + (1|ID), bacteria, binomial,
+                            method = 'Laplace'), gc = TRUE)
+    (fm6a <- lmer(y ~ trt + wk2 + (1|ID), bacteria, binomial,
+                  method = 'AGQ'))
+    system.time(fm6a <- lmer(y ~ trt + wk2 + (1|ID), bacteria, binomial,
+                             method = 'AGQ'), gc = TRUE)
+}
+q('no')
+
