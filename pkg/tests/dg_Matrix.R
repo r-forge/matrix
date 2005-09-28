9460e31b62b0e3f165cd90b69b3c2dd163e796d5
maechler <unknown@noreply.com>
2005-09-28 08:57:56 +0000

    less test *files*
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/matrix@948 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/tests/dg_Matrix.R
+++ pkg/tests/dg_Matrix.R
@@ -40,4 +40,29 @@ for(i in 1:10) {
                             B %*% A, B %*% Acsc))
 }
 
+###--- dgTMatrix {was ./dgTMatrix.R } -------
+
+### Use ``non-unique'' versions of dgTMatrix objects
+
+N <- 200
+set.seed(1)
+i <- as.integer(round(runif (N, 0, 100)))
+j <- as.integer(3* rpois (N, lam=15))
+x <- round(rnorm(N), 2)
+which(duplicated(cbind(i,j))) # 8 index pairs are duplicated
+
+m1 <- new("dgTMatrix", Dim = c(max(i)+1:1, max(j)+1:1), i = i, j = j, x = x)
+mc <- as(m1, "dgCMatrix")
+m2 <- as(mc, "dgTMatrix")## the same as 'm1' but without duplicates
+
+stopifnot(!isTRUE(all.equal(m1, m2)),
+          all.equal(as(m1,"matrix"), as(m2,"matrix"), tol=1e-15),
+          all.equal(crossprod(m1), crossprod(m2), tol=1e-15),
+          identical(mc, as(m2, "dgCMatrix")))
+
+### -> uniq* functions now in ../R/Auxiliaries.R
+(t2 <- system.time(um2 <- Matrix:::uniq(m1)))
+
+
+
 proc.time() # for ``statistical reasons''
