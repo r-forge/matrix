38f700860781d0ddade642bdff760edb8b5b47c6
maechler <unknown@noreply.com>
2005-10-01 20:08:51 +0000

    add "self-documenting" argument names
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/matrix@958 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/Csparse.R
+++ pkg/R/Csparse.R
@@ -1,11 +1,13 @@
 setMethod("crossprod", signature(x = "CsparseMatrix", y = "missing"),
 	  function(x, y = NULL)
-          .Call("Csparse_crossprod", x, FALSE, FALSE, PACKAGE = "Matrix"))
+	  .Call("Csparse_crossprod", x, trans = FALSE, triplet = FALSE,
+		PACKAGE = "Matrix"))
 
 setMethod("t", signature(x = "CsparseMatrix"),
-          function(x)
-          .Call("Csparse_transpose", x, PACKAGE = "Matrix"))
-          
+	  function(x)
+	  .Call("Csparse_transpose", x, PACKAGE = "Matrix"))
+
 setMethod("tcrossprod", signature(x = "CsparseMatrix"),
 	  function(x)
-          .Call("Csparse_crossprod", x, TRUE, FALSE, PACKAGE = "Matrix"))
+	  .Call("Csparse_crossprod", x, trans = TRUE, triplet = FALSE,
+		PACKAGE = "Matrix"))
