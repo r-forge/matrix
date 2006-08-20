5089baaf45ad98963a16a54b8b39d3a1ba573f9f
bates <unknown@noreply.com>
2006-08-20 03:17:19 +0000

    Added methods for sparseQR for qr.qy, qr.qty, qr.coef, qr.resid and qr.fitted
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/matrix@1396 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/R/sparseQR.R
@@ -0,0 +1,70 @@
+## Methods for the sparse QR decomposition
+
+setMethod("qr.qy", signature(qr = "sparseQR", y = "dgeMatrix"),
+          function(qr, y) .Call(sparseQR_qty, qr, y, TRUE, FALSE),
+          valueClass = "dgeMatrix")
+
+setMethod("qr.qy", signature(qr = "sparseQR", y = "matrix"),
+          function(qr, y) .Call(sparseQR_qty, qr, y, FALSE, FALSE),
+          valueClass = "dgeMatrix")
+
+setMethod("qr.qy", signature(qr = "sparseQR", y = "numeric"),
+          function(qr, y) .Call(sparseQR_qty, qr, as.matrix(as.double(y)),
+                                TRUE, FALSE),
+          valueClass = "dgeMatrix")
+
+setMethod("qr.qty", signature(qr = "sparseQR", y = "dgeMatrix"),
+          function(qr, y) .Call(sparseQR_qty, qr, y, TRUE, TRUE),
+          valueClass = "dgeMatrix")
+
+setMethod("qr.qty", signature(qr = "sparseQR", y = "matrix"),
+          function(qr, y) .Call(sparseQR_qty, qr, y, FALSE, TRUE),
+          valueClass = "dgeMatrix")
+
+setMethod("qr.qty", signature(qr = "sparseQR", y = "numeric"),
+          function(qr, y) .Call(sparseQR_qty, qr, as.matrix(as.double(y)),
+                                FALSE, TRUE),
+          valueClass = "dgeMatrix")
+
+setMethod("qr.coef", signature(qr = "sparseQR", y = "dgeMatrix"),
+          function(qr, y) .Call(sparseQR_coef, qr, y, TRUE),
+          valueClass = "dgeMatrix")
+
+setMethod("qr.coef", signature(qr = "sparseQR", y = "matrix"),
+          function(qr, y) .Call(sparseQR_coef, qr, y, FALSE),
+          valueClass = "dgeMatrix")
+
+setMethod("qr.coef", signature(qr = "sparseQR", y = "numeric"),
+          function(qr, y) .Call(sparseQR_coef, qr,
+                                as.matrix(as.double(y)), FALSE),
+          valueClass = "dgeMatrix")
+
+setMethod("qr.resid", signature(qr = "sparseQR", y = "dgeMatrix"),
+          function(qr, y)
+          .Call(sparseQR_resid_fitted, qr, y, TRUE, TRUE),
+          valueClass = "dgeMatrix")
+
+setMethod("qr.resid", signature(qr = "sparseQR", y = "matrix"),
+          function(qr, y)
+          .Call(sparseQR_resid_fitted, qr, y, FALSE, TRUE),
+          valueClass = "dgeMatrix")
+
+setMethod("qr.resid", signature(qr = "sparseQR", y = "numeric"),
+          function(qr, y) .Call(sparseQR_resid_fitted, qr,
+                                as.matrix(as.double(y)), FALSE, TRUE),
+          valueClass = "dgeMatrix")
+
+setMethod("qr.fitted", signature(qr = "sparseQR", y = "dgeMatrix"),
+          function(qr, y, k)
+          .Call(sparseQR_resid_fitted, qr, y, TRUE, FALSE),
+          valueClass = "dgeMatrix")
+
+setMethod("qr.fitted", signature(qr = "sparseQR", y = "matrix"),
+          function(qr, y, k)
+          .Call(sparseQR_resid_fitted, qr, y, FALSE, FALSE),
+          valueClass = "dgeMatrix")
+
+setMethod("qr.fitted", signature(qr = "sparseQR", y = "numeric"),
+          function(qr, y, k) .Call(sparseQR_resid_fitted, qr,
+                                   as.matrix(as.double(y)), FALSE, FALSE),
+          valueClass = "dgeMatrix")
