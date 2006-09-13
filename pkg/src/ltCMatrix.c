de9122d5d4460b4d6582402e0bd8296f27e5badd
bates <unknown@noreply.com>
2006-09-13 17:02:50 +0000

    Remove redundant validation checks
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/matrix@1558 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/src/ltCMatrix.c
+++ pkg/src/ltCMatrix.c
@@ -11,20 +11,21 @@
  */
 SEXP ltCMatrix_validate(SEXP x)
 {
-    SEXP val = triangularMatrix_validate(x);
-    if(isString(val))
-	return(val);
-    else {
+    /* Because ltCMatrix inherits from triangularMatrix this is not necessary */
+/*     SEXP val = triangularMatrix_validate(x); */
+/*     if(isString(val)) */
+/* 	return(val); */
+/*     else { */
 	/* FIXME needed? ltC* inherits from lgC* which does this in validate*/
-	SEXP pslot = GET_SLOT(x, Matrix_pSym),
-	    islot = GET_SLOT(x, Matrix_iSym);
-	int
-	    ncol = length(pslot) - 1,
-	    *xp = INTEGER(pslot),
-	    *xi = INTEGER(islot);
+/* 	SEXP pslot = GET_SLOT(x, Matrix_pSym), */
+/* 	    islot = GET_SLOT(x, Matrix_iSym); */
+/* 	int */
+/* 	    ncol = length(pslot) - 1, */
+/* 	    *xp = INTEGER(pslot), */
+/* 	    *xi = INTEGER(islot); */
 	/* column sorting now done in Csparse_validate */
 /* 	if (csc_unsorted_columns(ncol, xp, xi)) */
 /* 	    csc_sort_columns(ncol, xp, xi, (double *) NULL); */
 	return ScalarLogical(1);
-    }
+/*     } */
 }
