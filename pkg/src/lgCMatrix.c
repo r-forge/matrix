1f66c8d691dcb01fc59ffc23f062f33ba8bf6ef0
bates <unknown@noreply.com>
2006-09-13 17:02:50 +0000

    Remove redundant validation checks
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/matrix@1558 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/src/lgCMatrix.c
+++ pkg/src/lgCMatrix.c
@@ -2,30 +2,31 @@
 
 SEXP lgCMatrix_validate(SEXP x)
 {
-    SEXP pslot = GET_SLOT(x, Matrix_pSym),
-	islot = GET_SLOT(x, Matrix_iSym);
-    int j,
-	ncol = length(pslot) - 1,
-	*dims = INTEGER(GET_SLOT(x, Matrix_DimSym)),
-	nrow,
-	*xp = INTEGER(pslot),
-	*xi = INTEGER(islot);
+    /* All this is now done in Csparse_validate */
+/*     SEXP pslot = GET_SLOT(x, Matrix_pSym), */
+/* 	islot = GET_SLOT(x, Matrix_iSym); */
+/*     int j, */
+/* 	ncol = length(pslot) - 1, */
+/* 	*dims = INTEGER(GET_SLOT(x, Matrix_DimSym)), */
+/* 	nrow, */
+/* 	*xp = INTEGER(pslot), */
+/* 	*xi = INTEGER(islot); */
 
-    nrow = dims[0];
-    if (length(pslot) <= 0)
-	return mkString(_("slot p must have length > 0"));
-    if (xp[0] != 0)
-	return mkString(_("first element of slot p must be zero"));
-    if (length(islot) != xp[ncol])
-	return mkString(_("last element of slot p must match length of slot i"));
-    for (j = 0; j < ncol; j++) {
-	if (xp[j] > xp[j+1])
-	    return mkString(_("slot p must be non-decreasing"));
-    }
-    for (j = 0; j < length(islot); j++) {
-	if (xi[j] < 0 || xi[j] >= nrow)
-	    return mkString(_("all row indices must be between 0 and nrow-1"));
-    }
+/*     nrow = dims[0]; */
+/*     if (length(pslot) <= 0) */
+/* 	return mkString(_("slot p must have length > 0")); */
+/*     if (xp[0] != 0) */
+/* 	return mkString(_("first element of slot p must be zero")); */
+/*     if (length(islot) != xp[ncol]) */
+/* 	return mkString(_("last element of slot p must match length of slot i")); */
+/*     for (j = 0; j < ncol; j++) { */
+/* 	if (xp[j] > xp[j+1]) */
+/* 	    return mkString(_("slot p must be non-decreasing")); */
+/*     } */
+/*     for (j = 0; j < length(islot); j++) { */
+/* 	if (xi[j] < 0 || xi[j] >= nrow) */
+/* 	    return mkString(_("all row indices must be between 0 and nrow-1")); */
+/*     } */
     /* column sorting now done in Csparse_validate */
 /*     if (csc_unsorted_columns(ncol, xp, xi)) */
 /* 	csc_sort_columns(ncol, xp, xi, (double *) NULL); */
