e93b0e064740677c21b355d526c29330779621b3
maechler <unknown@noreply.com>
2006-09-28 15:31:17 +0000

    "Compare" for "dMatrix"; plus a few small ones
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/matrix@1592 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/Tsparse.R
+++ pkg/R/Tsparse.R
@@ -193,10 +193,10 @@ replTmat <- function (x, i, j, value)
     sel <- ((m1 <- match(x@i, i1, nomatch=0)) > 0:0 &
             (m2 <- match(x@j, i2, nomatch=0)) > 0:0)
 
-    has.x <- any("x" == slotNames(x)) # i.e. *not* logical
+    has.x <- any("x" == slotNames(x)) # i.e. *not* nonzero-pattern
 
     ## the simplest case: for all Tsparse, even for i or j missing
-    if(all(value == 0)) { ## just drop the non-zero entries
+    if(all0(value)) { ## just drop the non-zero entries
 	if(any(sel)) { ## non-zero there
 	    x@i <- x@i[!sel]
 	    x@j <- x@j[!sel]
@@ -224,7 +224,7 @@ replTmat <- function (x, i, j, value)
         return(x)
     }
 
-    v0 <- 0 == (value <- rep(value, length = lenRepl))
+    v0 <- is0(value <- rep(value, length = lenRepl))
     ## value[1:lenRepl]:  which are structural 0 now, which not?
 
     if(any(sel)) {
