a82cd7e45f8da640b229b539d42f1c215d4cd206
bates <unknown@noreply.com>
2006-10-01 22:44:02 +0000

    Added initial (Rasch) version of mlirt
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/matrix@1607 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- branches/trunk-lme4/R/lmer.R
+++ branches/trunk-lme4/R/lmer.R
@@ -53,11 +53,10 @@ subbars <- function(term)
 	term[[2]] <- subbars(term[[2]])
 	return(term)
     }
-    stopifnot(length(term) == 3)
+    stopifnot(length(term) >= 3)
     if (is.call(term) && term[[1]] == as.name('|'))
 	term[[1]] <- as.name('+')
-    term[[2]] <- subbars(term[[2]])
-    term[[3]] <- subbars(term[[3]])
+    for (j in 2:length(term)) term[[j]] <- subbars(term[[j]])
     term
 }
 
@@ -331,8 +330,8 @@ lmer <- function(formula, data, family = gaussian,
                  model = TRUE, ...)
 {
     formula <- as.formula(formula)
-	  if (length(formula) < 3) stop("formula must be a two-sided formula")
-	  cv <- do.call("lmerControl", control)
+    if (length(formula) < 3) stop("formula must be a two-sided formula")
+    cv <- do.call("lmerControl", control)
 
     ## Establish model frame and fixed-effects model matrix and terms
     mc <- match.call()
@@ -1171,4 +1170,100 @@ hatTrace <- function(x)
 {
     stopifnot(is(x, "mer"))
     .Call(mer_hat_trace2, x)
+}
+
+mlirt <-
+    function(formula, data, family = binomial("probit"),
+             control = list(), start = NULL,
+             subset, weights, na.action = na.ignore, offset,
+             contrasts = NULL, model = FALSE, ...)
+{
+    formula <- as.formula(formula)
+    if (length(formula) < 3) stop("formula must be a two-sided formula")
+    cv <- do.call("lmerControl", control)
+    
+    ## Establish model frame and fixed-effects model matrix and terms
+    mc <- match.call()
+    fr <- lmerFrames(mc, formula, data, contrasts)
+    Y <- fr$Y
+
+    ## check for a binary matrix response
+    if (!is.matrix(Y) || !is.numeric(Y) ||
+        any(!(unique(as.vector(Y)) %in% c(0, 1, NA)))) 
+        stop("Response must be a binary, numeric matrix")
+    nr <- nrow(Y)
+    nc <- ncol(Y)
+    ## expand model frame etc according to the items
+    ind <- rep.int(1:nr, nc)
+    mf <- fr$mf[ind, ]
+    X <- cbind(fr$X[ind, ], contr.sum(nc)[rep(1:nc,each = nr),])
+    Y <- as.vector(Y)
+    offset <- fr$offset[ind]
+
+    mf$.subj <- gl(nr, 1, nr*nc)
+    form <- substitute(Y ~ base + (1|.subj), list(base = formula[[3]])) 
+    ## establish factor list and Ztl
+    FL <- lmerFactorList(form, mf)
+    cnames <- with(FL, c(lapply(Ztl, rownames), list(.fixed = colnames(X))))
+    nc <- with(FL, sapply(Ztl, nrow))
+    Zt <- with(FL, .Call(Zt_create1, fl, Ztl))
+    fl <- FL$fl
+
+    ## check and evaluate the family argument
+    if(is.character(family))
+        family <- get(family, mode = "function", envir = parent.frame())
+    if(is.function(family)) family <- family()
+    if(is.null(family$family)) {
+        print(family)
+        stop("'family' not recognized")
+    }
+    if (family$family != "binomial")
+        stop("family must be a binomial family")
+
+    ## initial fit of a glm to the fixed-effects only.
+    glmFit <- glm.fit(X, Y, weights = fl$weights[ind],
+                      offset = offset, family = family,
+                      intercept = attr(fr$mt, "intercept") > 0)
+    Y <- as.double(glmFit$y)
+    ## must do this after Y has possibly been reformulated
+    mer <- .Call(mer_create, fl, Zt, X, Y, 0, nc, cnames)
+    if (!is.null(start)) mer <- setOmega(mer, start)
+
+    gVerb <- getOption("verbose")
+
+    ## extract some of the components of glmFit
+    ## weights could have changed
+    weights <- glmFit$prior.weights
+    eta <- glmFit$linear.predictors
+    wtssqr <- weights * weights
+    linkinv <- quote(family$linkinv(eta))
+    mu.eta <- quote(family$mu.eta(eta))
+    mu <- family$linkinv(eta)
+    variance <- quote(family$variance(mu))
+    dev.resids <- quote(family$dev.resids(Y, mu, wtssqr))
+    doLMEopt <- quote(LMEopt(x = mer, value = cv))
+    mer@devComp[8] <- -mean(weights)
+    mer@status["glmm"] <- as.integer(2) # always use Laplace
+    GSpt <- .Call(glmer_init, environment())
+    PQLpars <- c(coef(glmFit), .Call(mer_coef, mer, 2))
+    fixInd <- seq(ncol(X))
+    ## pars[fixInd] == beta, pars[-fixInd] == theta
+    ## indicator of constrained parameters
+    const <- c(rep(FALSE, length(fixInd)),
+               unlist(lapply(mer@nc[seq(along = fl)],
+                             function(k) 1:((k*(k+1))/2) <= k)
+                      ))
+    devLaplace <- function(pars) .Call(glmer_devLaplace, pars, GSpt)
+
+    optimRes <- nlminb(PQLpars, devLaplace,
+                       lower = ifelse(const, 5e-10, -Inf),
+                       control = list(trace = cv$msVerbose,
+                       iter.max = cv$msMaxIter))
+    .Call(glmer_finalize, GSpt)
+    new("glmer",
+        new("lmer", mer,
+            frame = if (model) mf else data.frame(),
+            terms = mt, call = match.call()),
+        weights = weights,
+        family=family)
 }
