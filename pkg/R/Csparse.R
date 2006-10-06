d5c80b7569736e8e51a94836c7722adcecbddfe8
maechler <unknown@noreply.com>
2006-10-06 21:29:15 +0000

    use Csparse_drop at least once
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/matrix@1619 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/Csparse.R
+++ pkg/R/Csparse.R
@@ -146,7 +146,16 @@ replCmat <- function (x, i, j, value)
 	x[i, ] <- value
     else
 	x[i,j] <- value
-    as_CspClass(x, clx)
+
+    ## Careful: 'Csparse_drop' also drops triangularity,...
+    ## .Call(Csparse_drop, as_CspClass(x, clx), 0)
+
+    if(any(is0(x@x))) { ## drop all values that "happen to be 0"
+	## FIXME: Csparse_drop should do this
+	as_CspClass(.Call(Csparse_drop, as_CspClass(x, clx), 0),
+		    clx)
+    }
+    else as_CspClass(x, clx)
 }
 
 setReplaceMethod("[", signature(x = "CsparseMatrix", i = "index", j = "missing",
