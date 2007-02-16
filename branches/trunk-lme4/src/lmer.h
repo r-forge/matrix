f721931ea6cf9fec1342f1b7bbec3344a3eec914
bates <unknown@noreply.com>
2007-02-16 22:36:41 +0000

    Split lmer.c into lmer.c and lmer2.c
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/matrix@1788 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- branches/trunk-lme4/src/lmer.h
+++ branches/trunk-lme4/src/lmer.h
@@ -7,38 +7,26 @@
 SEXP mer_ECMEsteps(SEXP x, SEXP nsteps, SEXP Verbp);
 SEXP mer_MCMCsamp(SEXP x, SEXP savebp, SEXP nsampp, SEXP transp,
 		  SEXP verbose, SEXP deviance);
-SEXP mer2_MCMCsamp(SEXP x, SEXP savebp, SEXP nsampp, SEXP transp,
-		   SEXP verbose, SEXP deviance);
 SEXP mer_coef(SEXP x, SEXP pType);
 SEXP mer_coefGets(SEXP x, SEXP coef, SEXP pType);
-SEXP mer_create(SEXP fl, SEXP Zt, SEXP Xp, SEXP yp, SEXP REMLp, SEXP nc, SEXP cnames);
-SEXP mer2_create(SEXP fl, SEXP Zt, SEXP Xp, SEXP yp, SEXP REMLp,
-		SEXP nc, SEXP cnames, SEXP offset, SEXP wts);
+SEXP mer_create(SEXP fl, SEXP Zt, SEXP Xp, SEXP yp, SEXP REMLp,
+		SEXP nc, SEXP cnames);
 SEXP mer_dtCMatrix(SEXP x);
 SEXP mer_dtCMatrix_inv(SEXP x);
-SEXP mer2_deviance(SEXP x, SEXP which);
 SEXP mer_fitted(SEXP x);
 SEXP mer_fixef(SEXP x);
-SEXP mer2_getPars(SEXP x);
 SEXP mer_gradient(SEXP x, SEXP pType);
 SEXP mer_hat_trace(SEXP x);
 SEXP mer_hat_trace2(SEXP x);
 SEXP mer_initial(SEXP x);
 SEXP mer_isNested(SEXP x);
 SEXP mer_postVar(SEXP x);
-SEXP mer2_postVar(SEXP x);
 SEXP mer_ranef(SEXP x);
-SEXP mer2_ranef(SEXP x);
-SEXP mer2_setPars(SEXP x, SEXP pars);
 SEXP mer_sigma(SEXP x, SEXP REML);
-SEXP mer2_sigma(SEXP x, SEXP which);
 SEXP mer_simulate(SEXP x, SEXP nsimP);
-SEXP mer2_update_effects(SEXP x);
 SEXP mer_update_ZXy(SEXP x);
 SEXP mer_update_y(SEXP x, SEXP ynew);
 SEXP mer_validate(SEXP x);
-SEXP mer2_validate(SEXP x);
-SEXP mer2_vcov(SEXP x);
 
 SEXP Ztl_sparse(SEXP fl, SEXP Ztl);
 SEXP Zt_carryOver(SEXP f, SEXP Zt, SEXP tvar, SEXP discount);
