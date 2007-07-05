711a45b26d419d3b96f9709bde0f66fdad5ab0e6
bates <unknown@noreply.com>
2007-07-05 18:12:08 +0000

    Add the macro N_AS_CHM_DN to "alloca" the required amount of memory then call M_numeric_as_chm_dense.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/matrix@1950 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- branches/Matrix-APIchange/inst/include/Matrix.h
+++ branches/Matrix-APIchange/inst/include/Matrix.h
@@ -26,6 +26,7 @@ CHM_FR M_as_cholmod_factor(CHM_FR ans, SEXP x);
 #define AS_CHM_FR(x) M_as_cholmod_factor((CHM_FR)alloca(sizeof(cholmod_factor)), x )
 #define AS_CHM_SP(x) M_as_cholmod_sparse((CHM_SP)alloca(sizeof(cholmod_sparse)), x )
 #define AS_CHM_TR(x) M_as_cholmod_triplet((CHM_TR)alloca(sizeof(cholmod_triplet)), x )
+#define N_AS_CHM_DN(x,n) M_numeric_as_chm_dense((CHM_DN)alloca(sizeof(cholmod_dense)), x , n )
 
 SEXP M_chm_factor_to_SEXP(CHM_FR f, int dofree);
 SEXP M_chm_sparse_to_SEXP(CHM_SP a, int dofree, int uploT, int Rkind,
