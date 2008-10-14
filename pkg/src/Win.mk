87dca29e3d5caa1e6e7ce52f90494ba5edc9fb0c
mmaechler (Martin Maechler) <maechler@r-project.org>
2008-10-14 15:15:38 +0000

    Makefile changes for parallel make
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/matrix@2288 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/src/Win.mk
+++ pkg/src/Win.mk
@@ -1,7 +1,7 @@
 ## Note: This is included from ./Makefile.win , but also
 ## ----  as 'MkInclude' from several */Makefile s
 include $(RHOME)/src/gnuwin32/MkRules
-CFLAGS=$(PKG_CFLAGS) -O2 -D__VC__
-ALL_CFLAGS=$(PKG_CFLAGS) -O2
+CFLAGS=$(PKG_CFLAGS) -O3
+ALL_CFLAGS=$(PKG_CFLAGS) -O3
 CXXFLAGS=$(PKG_CXXFLAGS) -O2 
-ALL_CXXFLAGS=$(PKG_CXXFLAGS)
+ALL_CXXFLAGS=$(PKG_CXXFLAGS) -O2
