b7a0ea87ae1c1318195ea61347b7f80dd03bef4a
mmaechler (Martin Maechler) <maechler@r-project.org>
2008-10-16 17:18:41 +0000

    move sh- and make- "scripts" to subdirectory scripts/
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/matrix@2293 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/src/scripts/UFsparse_download.sh
+++ pkg/src/scripts/UFsparse_download.sh
@@ -1,11 +1,17 @@
 #!/bin/sh
-
+## Update Libraries from Tim Davis' University of Florida (UF) collection:
+#
+if [ ! -d ../src ]
+then echo 'Must run in Matrix/src/ !' ; exit 1
+fi
 ufl_URL=http://www.cise.ufl.edu/research/sparse
 wget $ufl_URL/amd/current/AMD.tar.gz
 wget $ufl_URL/cholmod/current/CHOLMOD.tar.gz
 wget $ufl_URL/colamd/current/COLAMD.tar.gz
 wget $ufl_URL/UFconfig/current/UFconfig.tar.gz
 wget $ufl_URL/SPQR/current/SPQR.tar.gz
+
+## 1) UFconfig ---------------------------------------------
   ## install UFconfig.h file (now needed by some UFsparse libraries)
 tar zxf UFconfig.tar.gz UFconfig/UFconfig.h UFconfig/README.txt
   ## Move the UFconfig/README.txt file to ../inst/doc/UFsparse/UFconfig.txt
@@ -14,11 +20,16 @@ mv UFconfig/README.txt ../inst/doc/UFsparse/UFconfig.txt
   ## environment variables but this name is embedded in some Makefiles
 touch UFconfig/UFconfig.mk
   ## install COLAMD/Source and COLAMD/Include directories
+
+## 2) COLAMD -----------------------------------------------
 tar zxf COLAMD.tar.gz COLAMD/Source/ COLAMD/Include/
+Rscript --vanilla -e 'source("scripts/fixup-fn.R")' -e 'fixup("COLAMD/Source/Makefile")'
   ## install documentation for COLAMD
 tar zxf COLAMD.tar.gz COLAMD/README.txt COLAMD/ChangeLog
 mv COLAMD/README.txt ../inst/doc/UFsparse/COLAMD.txt
   ## install AMD/Source and AMD/Include directories
+
+## 3) AMD --------------------------------------------------
 tar zxf AMD.tar.gz AMD/Source AMD/Include AMD/README.txt
   ## restore the AMD/Source/Makefile
 svn revert AMD/Source/Makefile
@@ -26,6 +37,8 @@ svn revert AMD/Source/Makefile
 mv AMD/README.txt ../inst/doc/UFsparse/AMD.txt
   ## remove Fortran source files and GNUMakefile
 rm AMD/Source/*.f AMD/Source/GNUmakefile
+
+## 4) CHOLMOD ----------------------------------------------
   ## install CHOLMOD source files
 for d in Check Cholesky Core Include Lib MatrixOps Modify Partition Supernodal
 do
@@ -35,15 +48,17 @@ done
 tar zxf ./CHOLMOD.tar.gz CHOLMOD/README.txt
 mv CHOLMOD/README.txt ../inst/doc/UFsparse/CHOLMOD.txt
 
-mv CHOLMOD/Lib/Makefile CHOLMOD/Lib/Makefile_CHOLMOD
+cp -p CHOLMOD/Lib/Makefile CHOLMOD/Lib/Makefile_CHOLMOD
+Rscript --vanilla -e 'source("scripts/fixup-fn.R")' -e 'fixup("CHOLMOD/Lib/Makefile")'
+mv    CHOLMOD/Lib/Makefile CHOLMOD/Lib/Makefile_pre
 svn revert CHOLMOD/Lib/Makefile
   ##
+ls -l CHOLMOD/Lib/Makefile_pre
+echo 'now diff CHOLMOD/Lib/Makefile with CHOLMOD/Lib/Makefile_pre'
+echo ' make changes as necessary, and then (later)'
+echo ' rm CHOLMOD/Lib/Makefile_*' ; echo
 
-ls -l CHOLMOD/Lib/Makefile_CHOLMOD
-echo 'now diff CHOLMOD/Lib/Makefile with .... Makefile_CHOLMOD'
-echo ' make changes as necessary, and then'
-echo ' rm CHOLMOD/Lib/Makefile_CHOLMOD'
-
+## 5) SPQR -------------------------------------------------
   ## install SPQR source files
 for d in Source Include Lib
 do
@@ -53,16 +68,15 @@ done
 tar zxf ./SPQR.tar.gz SPQR/README.txt
 mv SPQR/README.txt ../inst/doc/UFsparse/SPQR.txt
 
-mv SPQR/Lib/Makefile SPQR/Lib/Makefile_SPQR
-#svn revert SPQR/Lib/Makefile
+cp -p SPQR/Lib/Makefile SPQR/Lib/Makefile_SPQR
+Rscript --vanilla -e 'source("scripts/fixup-fn.R")' -e 'fixup("SPQR/Lib/Makefile")'
+mv    SPQR/Lib/Makefile SPQR/Lib/Makefile_pre
+svn revert SPQR/Lib/Makefile
   ##
+ls -l SPQR/Lib/Makefile_pre
+echo 'now diff SPQR/Lib/Makefile with SPQR/Lib/Makefile_pre'
+echo ' make changes as necessary, and then (later)'
+echo ' rm SPQR/Lib/Makefile_*' ; echo
 
-
-ls -l CHOLMOD/Lib/Makefile_CHOLMOD
-echo 'now diff CHOLMOD/Lib/Makefile with .... Makefile_CHOLMOD'
-echo ' make changes as necessary, and then'
-echo ' rm CHOLMOD/Lib/Makefile_CHOLMOD'
-
-  ## remove the downloaded tar files
+## ----- remove the downloaded tar files -------------------
 rm CHOLMOD.tar.gz AMD.tar.gz COLAMD.tar.gz UFconfig.tar.gz SPQR.tar.gz
-
