d68f8fbcabed961c633e54da2c22b0bfe09f7162
mmaechler (Martin Maechler) <maechler@r-project.org>
2009-07-17 20:01:12 +0000

    first cut on hint about future determinant(<Choleski>) change
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/matrix@2438 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/CHMfactor.R
+++ pkg/R/CHMfactor.R
@@ -74,6 +74,18 @@ setMethod("determinant", signature(x = "CHMfactor", logarithm = "missing"),
 setMethod("determinant", signature(x = "CHMfactor", logarithm = "logical"),
           function(x, logarithm, ...)
       {
+          ## Produce a once per session warning :
+          if((.e <- exists("det_CHMfactor.warn", envir = .MatrixEnv))
+             && get("det_CHMfactor.warn", envir = .MatrixEnv)) {
+              ## ok
+          } else if (! .e) {
+              assign("det_CHMfactor.warn", TRUE, envir = .MatrixEnv)
+              warning("The next version of the Matrix package will return a\n",
+	"determinant(L) instead of determinant(A), i.e., a\n",
+        "*DIFFERENT* value.\n",
+        " Do change your code, following http://matrix.r-forge.r-project.org\n")
+## packageDescription("Matrix")$Version >= package_version("0.999375-31")
+          } ## else  the variable exists but is FALSE --> no warning either
           ldet <- .Call(CHMfactor_ldetL2, x)
           modulus <- if (logarithm) ldet else exp(ldet)
           attr(modulus, "logarithm") <- logarithm
