ffb8062ed9c36e6f5b3006ded73def8cc4b99ce6
mmaechler (Martin Maechler) <maechler@r-project.org>
2009-11-02 11:54:23 +0000

    improve "[" a bit for large nnz - using less memory
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/matrix@2492 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/Tsparse.R
+++ pkg/R/Tsparse.R
@@ -210,6 +210,9 @@ setMethod("[", signature(x = "TsparseMatrix", i = "index", j = "missing",
 
 	      d <- x@Dim
 	      n <- d[1]
+              ## FIXME: 'm' is of length 'nnz' which can be large;
+              ##       we do only need  which(.$m > 0L) = which(sel) below...
+              ##-> would like C-based  which.match()
 	      ip <- .ind.prep(x@i, intI(i, n = n, dimnames(x)[[1]]))
 	      Di1 <- ip$li
 	      drop.it <- drop && (Di1 == 1L || d[2] == 1L)
@@ -222,10 +225,10 @@ setMethod("[", signature(x = "TsparseMatrix", i = "index", j = "missing",
 	      }
 	      x@Dim[1] <- Di1
 	      if(!is.null(ip$dn)) x@Dimnames[[1]] <- ip$dn
-	      sel <- ip$m > 0L
+	      sel <- seq_along(ip$m)[ip$m > 0L] # == which(match(....))
 	      x@i <- ip$m[sel] - 1L
 	      if(ip$anyDup) { ## duplicated rows selected: extend sel
-		  sel <- c(which(sel), ip$jm)
+		  sel <- c(sel, ip$jm)
 		  x@i <- c(x@i, ip$i.xtra)
 	      }
 	      x@j <- x@j[sel]
@@ -248,6 +251,9 @@ setMethod("[", signature(x = "TsparseMatrix", i = "missing", j = "index",
 
 	      d <- x@Dim
 	      n <- d[2]
+              ## FIXME: 'm' is of length 'nnz' which can be large;
+              ##       we do only need  which(.$m > 0L) = which(sel) below...
+              ##-> would like C-based  which.match()
 	      ip <- .ind.prep(x@j, intI(j, n = n, dimnames(x)[[2]]))
 	      Di2 <- ip$li
 	      drop.it <- drop && (d[1] == 1L || Di2 == 1L)
@@ -260,10 +266,10 @@ setMethod("[", signature(x = "TsparseMatrix", i = "missing", j = "index",
 	      }
 	      x@Dim[2] <- Di2
 	      if(!is.null(ip$dn)) x@Dimnames[[2]] <- ip$dn
-	      sel <- ip$m > 0L
+	      sel <- seq_along(ip$m)[ip$m > 0L] # == which(match(....))
 	      x@j <- ip$m[sel] - 1L
 	      if(ip$anyDup) { ## duplicated columns selected: extend sel
-		  sel <- c(which(sel), ip$jm)
+		  sel <- c(sel, ip$jm)
 		  x@j <- c(x@j, ip$i.xtra)
 	      }
 	      x@i <- x@i[sel]
@@ -324,6 +330,8 @@ setMethod("[", signature(x = "TsparseMatrix",
 		  ii <- intI(i, n = di[1], dn= dn[[1]])
 		  ij <- intI(j, n = di[2], dn= dn[[2]])
 	      }
+              ## FIXME ip1$m , ip2$m  of length 'nnz' can be large;
+              ## rather use which.match(), or which.match("first" & "second")
 	      ip1 <- .ind.prep(x@i, ii)
 	      ip2 <- .ind.prep(x@j, ij)
 	  }
@@ -332,11 +340,10 @@ setMethod("[", signature(x = "TsparseMatrix",
 	  x@Dimnames <- list(ip1$dn, ip2$dn)
 
 	  if(isSym) {
-	      sel <- ip1$m  &  ip2$m
+	      sel <- which(ip1$m  &  ip2$m)## FIXME
 	      ii <- ip1$m[sel] - 1L
 	      jj <- ip2$m[sel] - 1L
 	      if(anyDup) { ## careful algorithm --- TODO: in C
-		  sel <- which(sel)
 		  ## keep non-duplicated and "increment" for duplicated ones
 		  ij <- pmin(ii, jj)
 		  jj <- pmax(ii, jj) ; ii <- ij
@@ -388,7 +395,7 @@ setMethod("[", signature(x = "TsparseMatrix",
 	  else if(!ip1$anyDup && !ip2$anyDup) {
 	      ## "normal case":	 no duplicated indices (and not symmetric)
 
-	      sel <- ip1$m  &  ip2$m
+	      sel <- which(ip1$m  &  ip2$m)## = intersect(which(), which())
 	      x@i <- ip1$m[sel] - 1L
 	      x@j <- ip2$m[sel] - 1L
 	  }
@@ -398,10 +405,10 @@ setMethod("[", signature(x = "TsparseMatrix",
 	      ## FIXME: we are recomputing ip2 here
 
 	      ## - i - ------------------------------
-	      sel <- ip1$m > 0L
+	      sel <- which(ip1$m > 0L)
 	      x@i <- ip1$m[sel] - 1L
 	      if(ip1$anyDup) { ## duplicated rows selected: extend sel
-		  sel <- c(which(sel), ip1$jm)
+		  sel <- c(sel, ip1$jm)
 		  x@i <- c(x@i, ip1$i.xtra)
 	      }
 	      x@j <- x@j[sel]
@@ -411,10 +418,10 @@ setMethod("[", signature(x = "TsparseMatrix",
 	      ## ip2 <- .ind.prep(x@j, intI(j, n = di[2], dn = dn[[2]]))
 	      ## FIXME can we do better: current x@j is original x@j[sel]
 	      ip2 <- .ind.prep(x@j, ij)
-	      sel <- ip2$m > 0L
+	      sel <- which(ip2$m > 0L)
 	      x@j <- ip2$m[sel] - 1L
 	      if(ip2$anyDup) { ## duplicated columns selected: extend sel
-		  sel <- c(which(sel), ip2$jm)
+		  sel <- c(sel, ip2$jm)
 		  x@j <- c(x@j, ip2$i.xtra)
 	      }
 	      x@i <- x@i[sel]
