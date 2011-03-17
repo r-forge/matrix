adf940b0bb73dc3ca1c94514374b8ca73553ae4f
mmaechler (Martin Maechler) <maechler@r-project.org>
2011-03-17 11:36:21 +0000

    merge r2656 (Sweave workaround) from "trunk" and make ready for release of Matrix version 0.999375-48 - for R-2.13.0 alpha
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/matrix@2657 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ branches/Matrix-for-R-2.13/data/wrld_1deg.R
@@ -0,0 +1,17 @@
+stopifnot(require(Matrix)) # at least its classes
+
+wrld_1deg <-
+    local({
+	load(system.file(file.path("external", "wrld_1deg_slots.rda"),
+                         package = "Matrix"))
+	## -> 'L'
+	r <- new("dsCMatrix")
+	for (n in c("Dim", "i","p","x"))
+	    slot(r, n) <- L[[n]]
+	r
+    })
+
+## The reverse:
+##  L <- list()
+##  for (n in c("Dim", "i","p","x"))    L[[n]] <- slot(wrld_1deg, n)
+
