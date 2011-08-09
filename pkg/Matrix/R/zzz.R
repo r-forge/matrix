c87b15af2dfaab9f8f912999ea0082ff64f356cf
mmaechler (Martin Maechler) <maechler@r-project.org>
2011-08-09 19:43:57 +0000

    make sure assignInNamspace is "there"
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/matrix@2695 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/Matrix/R/zzz.R
+++ pkg/Matrix/R/zzz.R
@@ -12,9 +12,6 @@
 
 .onLoad <- function(libname, pkgname)
 {
-    ## We now 'depend' on these anyway
-    ## require(methods)
-    ## require(utils) # -> assignInNamespace {but "anyway"}
 
     ## GOAL: all the functions in 'base' that start with something like
     ##	"x <- as.matrix(x)" or	"X <- as.array(X)"
