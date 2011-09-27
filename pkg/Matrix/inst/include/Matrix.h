88425b74f0f7e1b514387fc3c2a2e96ff43edc87
mmaechler (Martin Maechler) <maechler@r-project.org>
2011-09-27 14:56:52 +0000

    Matrix.h and *_stubs.c mismatch -- detected by Henrik Friberg
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/matrix@2715 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/Matrix/inst/include/Matrix.h
+++ pkg/Matrix/inst/include/Matrix.h
@@ -15,31 +15,31 @@ extern "C" {
 #endif
 
 // Copied from ../../src/Mutils.h ----------------------------------------
-#define MATRIX_valid_dense			\
+#define MATRIX_VALID_dense			\
         "dmatrix", "dgeMatrix",			\
 	"lmatrix", "lgeMatrix",			\
 	"nmatrix", "ngeMatrix",			\
 	"zmatrix", "zgeMatrix"
 
-#define MATRIX_valid_Csparse			\
+#define MATRIX_VALID_Csparse			\
  "dgCMatrix", "dsCMatrix", "dtCMatrix",		\
  "lgCMatrix", "lsCMatrix", "ltCMatrix",		\
  "ngCMatrix", "nsCMatrix", "ntCMatrix",		\
  "zgCMatrix", "zsCMatrix", "ztCMatrix"
 
-#define MATRIX_valid_Tsparse			\
+#define MATRIX_VALID_Tsparse			\
  "dgTMatrix", "dsTMatrix", "dtTMatrix",		\
  "lgTMatrix", "lsTMatrix", "ltTMatrix",		\
  "ngTMatrix", "nsTMatrix", "ntTMatrix",		\
  "zgTMatrix", "zsTMatrix", "ztTMatrix"
 
-#define MATRIX_valid_Rsparse			\
+#define MATRIX_VALID_Rsparse			\
  "dgRMatrix", "dsRMatrix", "dtRMatrix",		\
  "lgRMatrix", "lsRMatrix", "ltRMatrix",		\
  "ngRMatrix", "nsRMatrix", "ntRMatrix",		\
  "zgRMatrix", "zsRMatrix", "ztRMatrix"
 
-#define MATRIX_valid_CHMfactor "dCHMsuper", "dCHMsimpl", "nCHMsuper", "nCHMsimpl"
+#define MATRIX_VALID_CHMfactor "dCHMsuper", "dCHMsimpl", "nCHMsuper", "nCHMsimpl"
 
 CHM_SP M_as_cholmod_sparse (CHM_SP ans, SEXP x, Rboolean check_Udiag, Rboolean sort_in_place);
 CHM_TR M_as_cholmod_triplet(CHM_TR ans, SEXP x, Rboolean check_Udiag);
@@ -47,7 +47,7 @@ CHM_DN M_as_cholmod_dense(CHM_DN ans, SEXP x);
 CHM_DN M_numeric_as_chm_dense(CHM_DN ans, double *v, int nr, int nc);
 CHM_FR M_as_cholmod_factor(CHM_FR ans, SEXP x);
 double M_chm_factor_ldetL2(const_CHM_FR f);
-CHM_FR M_chm_factor_update(CHM_FR f, CHM_SP A, double mult);
+CHM_FR M_chm_factor_update(CHM_FR f, const_CHM_SP A, double mult);
 
 #define AS_CHM_DN(x) M_as_cholmod_dense((CHM_DN)alloca(sizeof(cholmod_dense)), x )
 #define AS_CHM_FR(x) M_as_cholmod_factor((CHM_FR)alloca(sizeof(cholmod_factor)), x )
@@ -61,10 +61,10 @@ CHM_FR M_chm_factor_update(CHM_FR f, CHM_SP A, double mult);
 #define N_AS_CHM_DN(x,nr,nc) M_numeric_as_chm_dense((CHM_DN)alloca(sizeof(cholmod_dense)), x , nr, nc )
 
 SEXP M_Csparse_diagU2N(SEXP x);
-SEXP M_chm_factor_to_SEXP(CHM_FR f, int dofree);
-SEXP M_chm_sparse_to_SEXP(CHM_SP a, int dofree, int uploT, int Rkind,
-			  char *diag, SEXP dn);
-SEXP M_chm_triplet_to_SEXP(CHM_TR a, int dofree, int uploT, int Rkind,
+SEXP M_chm_factor_to_SEXP(const_CHM_FR f, int dofree);
+SEXP M_chm_sparse_to_SEXP(const_CHM_SP a, int dofree, int uploT, int Rkind,
+			  const char *diag, SEXP dn);
+SEXP M_chm_triplet_to_SEXP(const CHM_TR a, int dofree, int uploT, int Rkind,
 			   const char* diag, SEXP dn);
 
 SEXP M_dpoMatrix_chol(SEXP x);
