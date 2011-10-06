c4014b4fc3bd5c0487f856f9796c285cfd232106
mmaechler (Martin Maechler) <maechler@r-project.org>
2011-10-06 10:17:19 +0000

    add a "memo" so I do not have to re-invent the wheel
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/matrix@2719 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ www/doxygen/UPDATE_me.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+## SVN_MATRIX_DIR is the directory produced by something like
+## svn co svn+ssh://mmaechler@svn.r-forge.r-project.org/svnroot/matrix Matrix
+SVN_MATRIX_DIR=~/R/D/R-forge/Matrix
+##
+checkDir = function () {
+    dir="$1"
+    Nam="$2"
+    if [ ! -d $dir ]
+    then
+	if [ x$Nam != x ] ; then echo -n "$Nam="; fi
+	echo "'$dir' is not a valid directory"
+    fi
+}
+
+checkDir $SVN_MATRIX_DIR SVN_MATRIX_DIR
+d=$SVN_MATRIX_DIR/pkg/Matrix/inst ; checkDir $d ; cd $d
+
+doxygen -u Doxyfile
+doxygen
+d=$SVN_MATRIX_DIR/www/doxygen; checkDir $d ; cd $d
+find . -path ./.svn -prune -o \( -mtime +1 -a -type f -exec rm {} \; \)
+svn add *
+
+cd $SVN_MATRIX_DIR
+svn ci -m'after "doxygen -u" and "doxygen" and cleanup of www directory' pkg/Matrix/inst/Doxyfile www/doxygen
