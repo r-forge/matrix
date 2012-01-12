0b2c0cba9b15f3f10251ede5b805e622c4324311
mmaechler (Martin Maechler) <maechler@r-project.org>
2012-01-12 11:57:48 +0000

    need Rversion.h  when checking for R_VERSION etc
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/matrix@2746 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/Matrix/inst/include/Matrix_stubs.c
+++ pkg/Matrix/inst/include/Matrix_stubs.c
@@ -1,6 +1,7 @@
 #include <Rconfig.h>
 #include <Rinternals.h>
 #include <R_ext/Rdynload.h>
+#include <Rversion.h>
 #include "cholmod.h"
 #include "Matrix.h"
 
@@ -615,7 +616,7 @@ M_cholmod_scale(const_CHM_DN S, int scale, CHM_SP A,
 // for now still *export*  M_Matrix_check_class_etc() -- deprecate it later__FIXME__
 int M_Matrix_check_class_etc(SEXP x, const char **valid)
 {
-#if R_VERSION < R_Version(2, 15, 0)
+#if R_VERSION < R_Version(2, 15, 0) // || R_SVN_REVISION < 57849
     static int(*fun)(SEXP, const char**) = NULL;
     if (fun == NULL)
 	fun = (int(*)(SEXP, const char**))
