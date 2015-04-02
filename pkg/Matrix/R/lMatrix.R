e5a491a744b92e5368b65a4014ddfe2a41585dcc
mmaechler (Martin Maechler) <maechler@r-project.org>
2015-04-02 08:29:03 +0000

    as(*, "nMatrix") now is allowed also for dense matrices with NA`s
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/matrix@3088 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/Matrix/R/lMatrix.R
+++ pkg/Matrix/R/lMatrix.R
@@ -4,20 +4,23 @@ setAs("matrix", "lMatrix",
 ## NOTE: This is *VERY* parallel to  ("dMatrix" -> "nMatrix") in ./dMatrix.R :
 setAs("lMatrix", "nMatrix",
       function(from) {
-	  if(any(is.na(from@x)))
-	      stop("\"lMatrix\" object with NAs cannot be coerced to \"nMatrix\"")
-	  ## i.e. from@x are only TRUE or FALSE
+	  if(anyNA(from@x) && ((.w <- isTRUE(getOption("Matrix.warn"))) ||
+				   isTRUE(getOption("Matrix.verbose")))) {
+	      (if(.w) warning else message)(
+		  "\"lMatrix\" object with NAs coerced to \"nMatrix\":  NA |-> TRUE")
+	      from@x[is.na(from@x)] <- TRUE
+	  }
+	  ## ==> from@x are in {TRUE, FALSE}
 	  cld <- getClassDef(cl <- MatrixClass(class(from)))
-	  if(extends(cld, "diagonalMatrix")) { # have no "ndi*" etc class
-	      cl <- class(from <- as(from, "sparseMatrix"))
-	      isSp <- TRUE
-	  } else {
-	      isSp <- extends(cld, "sparseMatrix")
-	      if(isSp && !all(from@x)) {
-		  from <- drop0(from) # was drop0(from, cld)
-		  if(cl != (c. <- class(from)))
-		      cld <- getClassDef(cl <- c.)
-	      }
+	  if(extends(cld, "diagonalMatrix")) # no "ndi*" class
+	      ## should not happen, setAs(diagonalMatrix -> nMatrix) in ./diagMatrix.R:
+	      return(di2nMat(from))
+	  ## else
+	  isSp <- extends(cld, "sparseMatrix")
+	  if(isSp && !all(from@x)) {
+	      from <- drop0(from) # was drop0(from, cld)
+	      if(cl != (c. <- class(from)))
+		  cld <- getClassDef(cl <- c.)
 	  }
 	  sNams <- slotNames(cld)
 	  copyClass(from, sub("^l", "n", cl),
