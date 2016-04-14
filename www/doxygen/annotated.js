f1cf918f3c259e8324e7403ae6b6f451eba5c543
mmaechler (Martin Maechler) <maechler@r-project.org>
2016-04-14 16:07:15 +0000

    remove old doxygen files (should be unused!)
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/matrix@3173 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- www/doxygen/annotated.js
+++
@@ -1,7 +0,0 @@
-var annotated =
-[
-    [ "cs_dmperm_results", "structcs__dmperm__results.html", "structcs__dmperm__results" ],
-    [ "cs_numeric", "structcs__numeric.html", "structcs__numeric" ],
-    [ "cs_sparse", "structcs__sparse.html", "structcs__sparse" ],
-    [ "cs_symbolic", "structcs__symbolic.html", "structcs__symbolic" ]
-];
