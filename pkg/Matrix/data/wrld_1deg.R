0a9c39e699ab7872e98aca8470d484f688c01f55
mmaechler (Martin Maechler) <maechler@r-project.org>
2019-02-14 17:15:34 +0000

    data() calling R code no longer attach()es
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/matrix@3285 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/Matrix/data/wrld_1deg.R
+++ pkg/Matrix/data/wrld_1deg.R
@@ -1,17 +1,18 @@
-stopifnot(require(Matrix), require(methods)) # Matrix classes; new, slot<-
+stopifnot(requireNamespace("methods", quietly = TRUE),
+	  requireNamespace("Matrix" , quietly = TRUE))
 
 wrld_1deg <-
     local({
 	load(system.file(file.path("external", "wrld_1deg_slots.rda"),
                          package = "Matrix"))
 	## -> 'L'
-	r <- new("dsCMatrix")
+	r <- methods::new("dsCMatrix")
 	for (n in c("Dim", "i","p","x"))
-	    slot(r, n) <- L[[n]]
+	    methods::slot(r, n) <- L[[n]]
 	r
     })
 
-## The reverse:
-##  L <- list()
-##  for (n in c("Dim", "i","p","x"))    L[[n]] <- slot(wrld_1deg, n)
-
+if(FALSE) {## The reverse:
+ L <- list()
+ for (n in c("Dim", "i","p","x"))    L[[n]] <- slot(wrld_1deg, n)
+}
