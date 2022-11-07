698077e071d6e2789b18a6f91c2c99d093303754
jaganmn (Mikael Jagan) <jaganmn@mcmaster.ca>
2022-11-07 23:06:36 +0000

    preliminary expand(<p?BunchKaufman>)
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/matrix@3759 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/Matrix/src/factorizations.h
+++ pkg/Matrix/src/factorizations.h
@@ -3,12 +3,13 @@
 
 #include "Mutils.h"
 
+SEXP denseLU_expand(SEXP obj);
+SEXP BunchKaufman_expand(SEXP obj);
+
 /* MJ: no longer needed ... replacement in ./validity.c */
 #if 0
 SEXP LU_validate(SEXP obj);
 #endif /* MJ */
-
-SEXP denseLU_expand(SEXP obj);
 
 /* MJ: no longer needed ... prefer denseLU_expand() */
 #if 0
