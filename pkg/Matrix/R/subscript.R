3e3b16a244d07c38719c39f422911cdf4a7ac030
jaganmn (Mikael Jagan) <jaganmn@mcmaster.ca>
2022-11-22 09:56:27 +0000

    staged subscript processing
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/matrix@3783 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/Matrix/R/subscript.R
+++ pkg/Matrix/R/subscript.R
@@ -33,13 +33,16 @@
         gettextf("invalid subscript type \"%s\"", typeof(i))
 }
 
+## x[i] where 'i' is NULL or any vector or sparseVector
 .subscript.1ary <- function(x, i) {
-    if(isS4(i)) {
+    mn <- prod(x@Dim)
+    if(is.null(i))
+        i <- integer(0L)
+    else if(isS4(i)) {
         if(!is(i, "sparseVector"))
             stop(.subscript.error.ist(i), domain = NA)
         if(!(has.x <- .hasSlot(i, "x")) || is.logical(i.x <- i@x)) {
             ## [nl]sparseVector
-            mn <- prod(x@Dim)
             i.length <- i@length
             i.i <- i@i
             i <-
@@ -51,24 +54,25 @@
                     r <- ceiling(mn / i.length)
                     i.i <-
                         if(r * i.length <= .Machine$integer.max)
-                            rep.int(i.i, r) +
+                            rep.int(as.integer(i.i), r) +
                                 rep(seq.int(from = 0L,
-                                            by = i.length,
+                                            by = as.integer(i.length),
                                             length.out = r),
                                     each = length(i.i))
-                        else
+                        else {
                             rep.int(as.double(i.i), r) +
                                 rep(seq.int(from = 0,
                                             by = as.double(i.length),
                                             length.out = r),
                                     each = length(i.i))
+                        }
                     if(has.x) {
                         if(r * i.length > mn) i.i[i.x & i.i <= mn] else i.i[i.x]
                     } else {
                         if(r * i.length > mn) i.i[      i.i <= mn] else i.i
                     }
                 }
-            return(.Call(R_subscript_1ary, x, i))
+            return(..subscript.1ary(x, i, unsorted = FALSE))
         }
         i <- i.x
     }
@@ -78,37 +82,35 @@
                    m <- min(1, i, na.rm = TRUE)
                    if(m < 1)
                        i <- if(m <= -1)
-                                seq_len(prod(x@Dim))[i]
+                                seq_len(mn)[i] # FIXME
                             else {
                                 if(is.object(i))
                                     i <- as.double(i)
                                 i[i >= 1]
                             }
-                   .Call(R_subscript_1ary, x, i)
+                   ..subscript.1ary(x, i)
                },
            integer =
                {
                    m <- min(1L, i, na.rm = TRUE)
                    if(m < 1L)
                        i <- if(m <= -1L)
-                                seq_len(prod(x@Dim))[i]
+                                seq_len(mn)[i] # FIXME
                             else {
                                 if(is.object(i))
                                     i <- as.integer(i)
                                 i[i >= 1L]
                             }
-                   .Call(R_subscript_1ary, x, i)
+                   ..subscript.1ary(x, i)
                },
            logical =
                {
                    if(!is.na(a <- all(i)) && a) {
-                       x <- as.vector(x)
-                       if((len <- length(i)) <= (mn <- prod(x@Dim)))
-                           return(x)
-                       else return(c(x, rep.int(NA, len - mn)))
+                       if((len <- length(i)) <= mn)
+                           return(as.vector(x))
+                       else return(c(as.vector(x), rep.int(NA, len - mn)))
                    }
-                   i <- seq_len(prod(x@Dim))[i]
-                   .Call(R_subscript_1ary, x, i)
+                   .subscript.1ary(x, as(i, "sparseVector")) # recursively
                },
            character =
                {
@@ -118,13 +120,59 @@
            stop(.subscript.error.ist(i), domain = NA))
 }
 
+## x[i] where 'i' is vector of type "integer" or "double"
+## with elements greater than or equal to 1 (or NA)
+..subscript.1ary <- function(x, i,
+                             shape = .M.shape(x),
+                             repr = .M.repr(x),
+                             unsorted = is.unsorted(i)) {
+    if(!nzchar(repr))
+        return(.Call(R_subscript_1ary, x, i))
+    if(repr == "R" || shape == "s") {
+        d <- x@Dim
+        m <- d[1L]
+        mn. <- min(prod(d), 0x1p+52) # R_XLEN_T_MAX
+        if(max(mn., i, na.rm = TRUE) >= mn. + 1)
+            i[i >= mn. + 1] <- NA
+        i1s <- i - 1L
+        i. <- as.integer(i1s %% m)
+        if(shape == "s") {
+            j. <- as.integer(i1s %/% m)
+            op <- if(x@uplo == "U") `>` else `<`
+            if(length(w <- which(op(i., j.)))) {
+                tmp <- i.[w]
+                i.[w] <- j.[w]
+                j.[w] <- tmp
+            }
+            if (mn. > .Machine$integer.max)
+                m <- as.double(m)
+            i <- m * j. + i. + 1L
+        }
+    }
+    o <-
+        if(repr == "R")
+            order(i., i)
+        else if(is.na(unsorted) || unsorted)
+            sort.list(i)
+        else return(.Call(R_subscript_1ary, x, i))
+    if(is.unsorted(o)) {
+        s <- .Call(R_subscript_1ary, x, i[o])
+        s[o] <- tmp
+        s
+    } else .Call(R_subscript_1ary, x, i)
+}
+
+## x[i] where 'i' is any array or Matrix
 .subscript.1ary.mat <- function(x, i) {
     if(isS4(i)) {
         cld <- getClassDef(class(i))
         if(!extends(cld, "Matrix"))
             stop(.subscript.error.ist(i), domain = NA)
-        if((logic <- extends(cld, "nMatrix") || extends(cld, "lMatrix")) ||
-           i@Dim[2L] != 2L) {
+        logic <-
+            extends(cld, "nMatrix") ||
+            extends(cld, "lMatrix") ||
+            extends(cld, "indMatrix")
+        if(logic || i@Dim[2L] != 2L) {
             if(logic && !is.na(a <- all(i)) && a) {
                 x <- as.vector(x)
                 if((len <- prod(i@Dim)) <= (mn <- prod(x@Dim)))
@@ -141,18 +189,12 @@
            double =,
            integer =
                {
-                   ## * rows containing 0 are deleted
-                   ## * rows containing NA result in NA
-                   ## * rows containing both 0 and NA are handled
-                   ##   according to value in first column
                    storage.mode(i) <- "integer"
-                   i <- i[i[, 1L] != 0L, , drop = FALSE] # [NA,j] -> [NA,NA]
-                   i <- i[i[, 2L] != 0L, , drop = FALSE]
                    if(min(1L, i, na.rm = TRUE) < 1L)
                        stop(.subscript.error.neg)
-                   d <- x@Dim
-                   m <- d[1L]
-                   n <- d[2L]
+                   dx <- x@Dim
+                   m <- dx[1L]
+                   n <- dx[2L]
                    if(m == n) {
                        if(max(n, i, na.rm = TRUE) > n)
                            stop(.subscript.error.oob)
@@ -161,37 +203,80 @@
                           max(n, i[, 2L], na.rm = TRUE) > n)
                            stop(.subscript.error.oob)
                    }
+                   ## * rows containing 0 are deleted
+                   ## * rows containing NA are kept
+                   ## * rows containing both 0 and NA are handled
+                   ##   according to value in first column
+                   if(is.na(a <- all(i. <- i[, 1L])) || !a)
+                       i <- i[!i., , drop = FALSE]
+                   if(!all(j. <- i[, 2L], na.rm = TRUE))
+                       i <- i[!j., , drop = FALSE]
                    .Call(R_subscript_1ary_mat, x, i)
                },
            character =
                {
-                   dn <- dimnames(x)
-                   m <- c(match(i[, 1L], dn[[1L]]), match(i[, 2L], dn[[2L]]))
+                   dnx <- dimnames(x)
+                   m <- c(match(i[, 1L], dnx[[1L]]), match(i[, 2L], dnx[[2L]]))
                    dim(m) <- di
-                   if(any(rowSums(is.na(i)) == 0L & rowSums(is.na(m)) > 0L))
+                   if(any(!rowSums(is.na(i)) & rowSums(is.na(m))))
                        ## error if character row contains zero NA and
                        ## integer row contains at least one NA,
-                       ## indicating non-match that cannot be ignored
+                       ## indicating non-match that should not be ignored
                        stop(.subscript.error.oob)
                    .Call(R_subscript_1ary_mat, x, m)
                },
            stop(.subscript.error.ist(i), domain = NA))
 }
 
+## x[i] where 'i' is a 2-column matrix of type "integer"
+## with i[, 1L] in 1:m (or NA) and i[, 2L] in 1:n (or NA)
+..subscript.1ary.mat <- function(x, i,
+                                 shape = .M.shape(x),
+                                 repr = .M.repr(x)) {
+    if(!nzchar(repr))
+        return(.Call(R_subscript_1ary_mat, x, i))
+    i. <- i[, 1L]
+    j. <- i[, 2L]
+    if(shape == "s") {
+        op <- if(x@uplo == "U") `>` else `<`
+        if(length(w <- which(op(i., j.)))) {
+            tmp <- i.[w]
+            i.[w] <- j.[w]
+            j.[w] <- tmp
+        }
+    }
+    o <-
+        if(repr == "R") {
+            if(anyNA(j.))
+                i.[is.na(j.)] <- NA
+            order(i., j.)
+        } else {
+            if(anyNA(i.))
+                j.[is.na(i.)] <- NA
+            order(j., i.)
+        }
+    if(is.unsorted(o)) {
+        s <- .Call(R_subscript_1ary_mat, x, i[o, , drop = FALSE])
+        s[o] <- s
+        s
+    } else .Call(R_subscript_1ary_mat, x, i)
+}
+
+## x[i, j, drop] where 'i' and 'j' are NULL or any vector
 .subscript.2ary <- function(x, i, j, drop) {
     d <- x@Dim
-    index <- list(if(missing(i)) NULL else if(is.null(i)) integer(0L) else i,
-                  if(missing(j)) NULL else if(is.null(j)) integer(0L) else j)
+    i <- list(if(missing(i)) NULL else if(is.null(i)) integer(0L) else i,
+              if(missing(j)) NULL else if(is.null(j)) integer(0L) else j)
     for(pos in 1:2) {
-        if(!is.null(k <- index[[pos]])) {
-            index[[pos]] <-
+        if(!is.null(k <- i[[pos]])) {
+            i[[pos]] <-
                 switch(typeof(k),
                        double =
                            {
                                r <- d[pos]
                                if(is.object(k))
                                    k <- as.double(k)
-                               if(max(r + 1, k, na.rm = TRUE) > r + 1)
+                               if(max(r, k, na.rm = TRUE) >= r + 1)
                                    stop(.subscript.error.oob)
                                seq_len(r)[k]
                            },
@@ -202,7 +287,9 @@
                                    k <- as.integer(k)
                                if(max(r, k, na.rm = TRUE) > r)
                                    stop(.subscript.error.oob)
-                               seq_len(r)[k]
+                               if(min(1L, k, na.rm = TRUE) < 1L)
+                                   seq_len(r)[k]
+                               else r
                            },
                        logical =
                            {
@@ -223,8 +310,24 @@
                        stop(.subscript.error.ist(k), domain = NA))
         }
     }
-    .Call(R_subscript_2ary, x, index[[1L]], index[[2L]],
-          if(missing(drop)) TRUE else drop)
+    ..subscript.2ary(x, i, drop = is.na(drop <- drop[1L]) && drop)
+}
+
+## x[i[[1L]], i[[2L]], drop] where 'i' is a length-2 list
+## of vectors of type "integer" with i[[1L]] in 1:m (or NA)
+## and i[[2L]] in 1:n (or NA) ... NULL indicates missingness
+..subscript.2ary <- function(x, i, drop) {
+    mi. <- is.null(i. <- i[[1L]])
+    mj. <- is.null(j. <- j[[2L]])
+    if(mi. && mj.) {
+
+    } else if (mi.) {
+
+    } else if (mj.) {
+
+    } else {
+
+    }
 }
 
 setMethod("[", signature(x = "Matrix", i = "missing", j = "missing",
@@ -258,7 +361,7 @@ setMethod("[", signature(x = "Matrix", i = "missing", j = "missing",
                   x
               } else if(na == 4L) {
                   ## x[, , drop=], x[, drop=, ], x[drop=, , ]
-                  if(isFALSE(drop[1L])) x else drop(x)
+                  if(is.na(drop <- drop[1L]) || drop) drop(x) else x
               } else {
                   ## x[, , , drop=], etc.
                   stop(.subscript.error.dim)
