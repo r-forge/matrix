921fbe4c5568158562abff02380c810be225702a
jaganmn (Mikael Jagan) <jaganmn@mcmaster.ca>
2023-04-29 04:37:52 +0000

    a few more common sense methods for MF
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/matrix@3885 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/Matrix/R/MatrixFactorization.R
+++ pkg/Matrix/R/MatrixFactorization.R
@@ -2,11 +2,57 @@
 ## mother class containing all matrix factorizations
 ## ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-setMethod("dim", "MatrixFactorization",
+if(FALSE) {
+## MJ: These would make a lot of sense, but we already have
+##     * as(      <denseLU>, "Matrix")
+##     * as( <BunchKaufman>, "Matrix")
+##     * as(<pBunchKaufman>, "Matrix")
+##     * as(    <CHMfactor>, "Matrix")
+##     _not_ returning the factorized matrix ...
+##
+##     Remove those and replace them with more specific coercions ??
+##     * as(      <denseLU>, "dgeMatrix")
+##     * as( <BunchKaufman>, "dtrMatrix")
+##     * as(<pBunchKaufman>, "dtpMatrix")
+##     * as(    <CHMfactor>, "dtCMatrix")
+setAs("MatrixFactorization", "Matrix",
+      function(from) {
+          n <- length(x <- expand2(from))
+          to <- x[[1L]]
+          if(n >= 2L) for(i in 2:n) to <- to %*% x[[i]]
+          to
+      })
+
+setAs("MatrixFactorization", "matrix",
+      function(from) as(as(from, "Matrix"), "matrix"))
+}
+
+setMethod("dim", signature(x = "MatrixFactorization"),
           function(x) x@Dim)
 
-setMethod("dimnames", "MatrixFactorization",
+setMethod("length", "MatrixFactorization",
+          function(x) prod(x@Dim))
+
+setMethod("dimnames", signature(x = "MatrixFactorization"),
           function(x) x@Dimnames)
+
+setMethod("dimnames<-", signature(x = "MatrixFactorization", value = "list"),
+          function(x, value) {
+              x@Dimnames <- fixupDN.if.valid(value, x@Dim)
+              x
+          })
+
+setMethod("dimnames<-", signature(x = "MatrixFactorization", value = "NULL"),
+	  function(x, value) {
+              x@Dimnames <- list(NULL, NULL)
+              x
+          })
+
+setMethod("unname", signature(obj = "MatrixFactorization"),
+	  function(obj, force = FALSE) {
+              obj@Dimnames <- list(NULL, NULL)
+              obj
+          })
 
 setMethod("show", "MatrixFactorization",
 	  function(object) {
