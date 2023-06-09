592c86279790a665145b4098f439f06b154d586b
jaganmn (Mikael Jagan) <jaganmn@mcmaster.ca>
2023-06-09 04:24:30 +0000

    default values (for clarity)
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/matrix@4031 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/Matrix/R/which.R
+++ pkg/Matrix/R/which.R
@@ -2,14 +2,14 @@
 ## ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 setMethod("which", "ndenseMatrix",
-          function(x, arr.ind, useNames) {
+          function(x, arr.ind = FALSE, useNames = TRUE) {
               wh <- which(.dense2g(x, "l")@x) # NA <=> TRUE
               if(arr.ind)
                   arrayInd(wh, x@Dim, dimnames(x), useNames = useNames)
               else wh
           })
 setMethod("which", "ldenseMatrix",
-          function(x, arr.ind, useNames) {
+          function(x, arr.ind = FALSE, useNames = TRUE) {
               wh <- which(.dense2g(x, "l")@x)
               if(arr.ind)
                   arrayInd(wh, x@Dim, dimnames(x), useNames = useNames)
@@ -17,23 +17,23 @@ setMethod("which", "ldenseMatrix",
           })
 
 setMethod("which", "nsparseMatrix",
-	  function(x, arr.ind, useNames = TRUE) {
+	  function(x, arr.ind = FALSE, useNames = TRUE) {
               wh <- which(as(x, "sparseVector"))
               if(arr.ind)
                   arrayInd(wh, x@Dim, dimnames(x), useNames = useNames)
 	      else wh
 	  })
 setMethod("which", "lsparseMatrix",
-	  function(x, arr.ind, useNames = TRUE) {
+	  function(x, arr.ind = FALSE, useNames = TRUE) {
               wh <- which(as(x, "sparseVector"))
               if(arr.ind)
                   arrayInd(wh, x@Dim, dimnames(x), useNames = useNames)
 	      else wh
 	  })
 setMethod("which", "ldiMatrix",
-	  function(x, arr.ind, useNames) {
+	  function(x, arr.ind = FALSE, useNames = TRUE) {
               d <- x@Dim
-              wh <- indDiag(n = d[1L], packed = FALSE)
+              wh <- indDiag(d[1L])
               if(x@diag == "N")
                   wh <- wh[which(x@x)]
               if(arr.ind)
@@ -42,6 +42,6 @@ setMethod("which", "ldiMatrix",
           })
 
 setMethod("which", "nsparseVector",
-          function(x, arr.ind, useNames) x@i)
+          function(x, arr.ind = FALSE, useNames = TRUE) x@i)
 setMethod("which", "lsparseVector",
-          function(x, arr.ind, useNames) x@i[which(x@x)])
+          function(x, arr.ind = FALSE, useNames = TRUE) x@i[which(x@x)])
