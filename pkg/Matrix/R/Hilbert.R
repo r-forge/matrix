93fef49a4c6f14a941cf8ea1c980e75ef5ae01de
jaganmn (Mikael Jagan) <jaganmn@mcmaster.ca>
2023-08-16 04:20:26 +0000

    migrate "constructors" to R/construct.R
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/matrix@4247 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/Matrix/R/Hilbert.R
+++ pkg/Matrix/R/Hilbert.R
@@ -1,6 +1,1 @@
-## Generate the Hilbert matrix of dimension 'n' :
-Hilbert <- function(n) {
-    n <- as.integer(n)
-    i <- seq_len(n)
-    new("dpoMatrix", Dim = c(n, n), x = c(1/outer(i - 1L, i, `+`)))
-}
+## in .Rbuildignore since r4247, empty since r4247
