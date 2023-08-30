cf1a951881776c96fbe71e25fcdf767ce0b3fb98
jaganmn (Mikael Jagan) <jaganmn@mcmaster.ca>
2023-08-30 00:44:08 +0000

    less .empty.factors, .setZero; add mmultDN for C-level products
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/matrix@4292 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/Matrix/R/diagMatrix.R
+++ pkg/Matrix/R/diagMatrix.R
@@ -17,18 +17,38 @@
  .diag.x <- function(m) if(m@diag != "N") rep.int(as1(m@x), m@Dim[1L]) else m@x
 ..diag.x <- function(m)                   rep.int(as1(m@x), m@Dim[1L])
 
-
 setMethod("band", signature(x = "diagonalMatrix"),
-          function(x, k1, k2, ...)
-              if(k1 <= 0L && k2 >= 0L) x else .setZero(x))
+          function(x, k1, k2, ...) {
+              if(k1 <= 0L && k2 >= 0L)
+                  return(x)
+              r <- new(.M.nonvirtual(x))
+              r@Dim <- d <- x@Dim
+              r@Dimnames <- x@Dimnames
+              r@x <- vector(typeof(x@x), d[1L])
+              r
+          })
 
 setMethod("triu", signature(x = "diagonalMatrix"),
-          function(x, k = 0L, ...)
-              if(k <= 0L) x else .setZero(x))
+          function(x, k = 0L, ...) {
+              if(k <= 0L)
+                  return(x)
+              r <- new(.M.nonvirtual(x))
+              r@Dim <- d <- x@Dim
+              r@Dimnames <- x@Dimnames
+              r@x <- vector(typeof(x@x), d[1L])
+              r
+          })
 
 setMethod("tril", signature(x = "diagonalMatrix"),
-          function(x, k = 0L, ...)
-              if(k >= 0L) x else .setZero(x))
+          function(x, k = 0L, ...) {
+              if(k >= 0L)
+                  return(x)
+              r <- new(.M.nonvirtual(x))
+              r@Dim <- d <- x@Dim
+              r@Dimnames <- x@Dimnames
+              r@x <- vector(typeof(x@x), d[1L])
+              r
+          })
 
 setMethod("diag", signature(x = "diagonalMatrix"),
           function(x, nrow, ncol, names = TRUE) {
@@ -76,16 +96,42 @@ setMethod("t", signature(x = "diagonalMatrix"),
           function(x) { x@Dimnames <- x@Dimnames[2:1]; x })
 
 setMethod("forceSymmetric", signature(x = "diagonalMatrix", uplo = "missing"),
-          function(x, uplo) .diag2sparse(x, "s", "C", "U"))
+          function(x, uplo) .diag2sparse(x, "s", "C",  "U"))
 
 setMethod("forceSymmetric", signature(x = "diagonalMatrix", uplo = "character"),
           function(x, uplo) .diag2sparse(x, "s", "C", uplo))
 
 setMethod("symmpart", signature(x = "diagonalMatrix"),
-          function(x) forceSymmetric(.M2kind(x, "d")))
+          function(x) {
+              kind <- .M.kind(x)
+              r <- new(if(kind != "z") "ddiMatrix" else "zdiMatrix")
+              r@Dim <- x@Dim
+              r@Dimnames <- symmDN(x@Dimnames)
+              if(x@diag != "N")
+                  r@diag <- "U"
+              else
+                  r@x <- switch(kind,
+                                "n" = as.double(x@x | is.na(x@x)),
+                                "l" = ,
+                                "i" = ,
+                                "d" = as.double(x@x),
+                                "z" = complex(real = Re(x@x), imaginary = 0))
+              r
+          })
 
 setMethod("skewpart", signature(x = "diagonalMatrix"),
-          function(x) symmetrizeDN(.setZero(x, "d")))
+          function(x) {
+              kind <- .M.kind(x)
+              r <- new(if(kind != "z") "ddiMatrix" else "zdiMatrix")
+              r@Dim <- d <- x@Dim
+              r@Dimnames <- symmDN(x@Dimnames)
+              r@x <- if(kind != "z")
+                         double(d[1L])
+                     else if(x@diag != "N")
+                         complex(d[1L])
+                     else complex(real = 0, imaginary = Im(x@x))
+              r
+          })
 
 setMethod("isDiagonal", signature(object = "diagonalMatrix"),
           function(object) TRUE)
