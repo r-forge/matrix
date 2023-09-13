d4af07eb8ff974508b3677fcc09e7476f1bd4086
jaganmn (Mikael Jagan) <jaganmn@mcmaster.ca>
2023-09-13 23:51:33 +0000

    clean up Summary, Math, Math2, zapsmall methods
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/matrix@4327 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/Matrix/tests/group-methods.R
+++ pkg/Matrix/tests/group-methods.R
@@ -572,4 +572,25 @@ sinu <- `dimnames<-`(sin(diag(2L)), Dn)
 for(u in list(udi, utC, utr))
     stopifnot(identical(as(sin(u), "matrix"), sinu))
 
+## Originally in ../man/all-methods.Rd :
+M <- Matrix(1:12 +0, 3,4)
+all(M >= 1) # TRUE
+any(M < 0 ) # FALSE
+MN <- M; MN[2,3] <- NA; MN
+all(MN >= 0) # NA
+any(MN <  0) # NA
+any(MN <  0, na.rm = TRUE) # -> FALSE
+sM <- as(MN, "sparseMatrix")
+stopifnot(all(M >= 1), !any(M < 0),
+          all.equal((sM >= 1), as(MN >= 1, "sparseMatrix")),
+          ## MN:
+          any(MN < 2), !all(MN < 5),
+          is.na(all(MN >= 0)), is.na(any(MN < 0)),
+          all(MN >= 0, na.rm=TRUE), !any(MN < 0, na.rm=TRUE),
+          ## same for sM :
+          any(sM < 2), !all(sM < 5),
+          is.na(all(sM >= 0)), is.na(any(sM < 0)),
+          all(sM >= 0, na.rm=TRUE), !any(sM < 0, na.rm=TRUE)
+         )
+
 cat('Time elapsed: ', proc.time(),'\n') # for ``statistical reasons''
