0c286ee6b5c160de42190a0930babf6712ecb896
jaganmn (Mikael Jagan) <jaganmn@mcmaster.ca>
2023-09-20 19:22:09 +0000

    prepare 'norm', 'rcond' code for zMatrix
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/matrix@4358 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/Matrix/R/rcond.R
+++ pkg/Matrix/R/rcond.R
@@ -4,6 +4,75 @@
 setMethod("rcond", signature(x = "ANY", norm = "missing"),
           function(x, norm, ...) rcond(x, norm = "O", ...))
 
+setMethod("rcond", signature(x = "denseMatrix", norm = "character"),
+          function(x, norm, ...) {
+              cl <- .M.nonvirtual(x, strict = TRUE)
+              if(any(substr(cl, 1L, 1L) == c("n", "l", "i")))
+                  x <- .M2kind(x, "d")
+              switch(substr(cl, 2L, 3L),
+                     "ge" =
+                         {
+                             d <- x@Dim
+                             m <- d[1L]
+                             n <- d[2L]
+                             if(m == n) {
+                                 trf <- lu(x, warnSing = FALSE)
+                                 .Call(dgeMatrix_rcond, x, trf, norm)
+                             } else {
+                                 ## MJ: norm(A = P1' Q R P2') = norm(R) holds
+                                 ##     in general only for norm == "2", but
+                                 ##     La_rcond_type() disallows norm == "2"
+                                 ##     ... FIXME ??
+                                 if(m < n) {
+                                     x <- t(x)
+                                     n <- m
+                                 }
+                                 R <- triu(qr(x)[["qr"]][seq_len(n), , drop = FALSE])
+                                 rcond(R, norm = norm, ...)
+                             }
+                         },
+                     "sy" =
+                         {
+                             trf <- BunchKaufman(x, warnSing = FALSE)
+                             .Call(dsyMatrix_rcond, x, trf, norm)
+                         },
+                     "sp" =
+                         {
+                             trf <- BunchKaufman(x, warnSing = FALSE)
+                             .Call(dspMatrix_rcond, x, trf, norm)
+                         },
+                     "po" = ,
+                     "or" = # corMatrix
+                         {
+                             ok <- TRUE
+                             trf <- tryCatch(
+                                 Cholesky(x, perm = FALSE),
+                                 error = function(e) {
+                                     ok <<- FALSE
+                                     BunchKaufman(x, warnSing = FALSE)
+                                 })
+                             if(ok)
+                                 .Call(dpoMatrix_rcond, x, trf, norm)
+                             else .Call(dsyMatrix_rcond, x, trf, norm)
+                         },
+                     "pp" = ,
+                     "co" = # pcorMatrix
+                         {
+                             ok <- TRUE
+                             trf <- tryCatch(
+                                 Cholesky(x, perm = FALSE),
+                                 error = function(e) {
+                                     ok <<- FALSE
+                                     BunchKaufman(x, warnSing = FALSE)
+                                 })
+                             if(ok)
+                                 .Call(dppMatrix_rcond, x, trf, norm)
+                             else .Call(dspMatrix_rcond, x, trf, norm)
+                         },
+                     "tr" = .Call(dtrMatrix_rcond, x, norm),
+                     "tp" = .Call(dtpMatrix_rcond, x, norm))
+          })
+
 setMethod("rcond", signature(x = "sparseMatrix", norm = "character"),
           function(x, norm, useInv = FALSE, ...) {
               d <- x@Dim
@@ -53,6 +122,8 @@ setMethod("rcond", signature(x = "diagonalMatrix", norm = "character"),
                          } else 1,
                      "F" = , "f" = , "E" = , "e" =
                          if(nonunit) {
+                             if(is.complex(y))
+                                 y <- abs(y)
                              yy <- y * y
                              1 / sqrt(sum(yy) * sum(1 / yy))
                          } else 1 / n,
@@ -102,77 +173,4 @@ setMethod("rcond", signature(x = "pMatrix", norm = "character"),
                          1 / n,
                      stop(gettext("invalid %s=\"%s\"", "norm", norm[1L]),
                           domain = NA))
-          })
-
-setMethod("rcond", signature(x = "denseMatrix", norm = "character"),
-          function(x, norm, ...)
-              rcond(.M2kind(x, "d"), norm = norm, ...))
-
-setMethod("rcond", signature(x = "dgeMatrix", norm = "character"),
-          function(x, norm, ...) {
-              d <- x@Dim
-              m <- d[1L]
-              n <- d[2L]
-              if(m == n) {
-                  trf <- lu(x, warnSing = FALSE)
-                  .Call(dgeMatrix_rcond, x, trf, norm)
-              } else {
-                  ## MJ: norm(A = P1' Q R P2') = norm(R) holds in general
-                  ##     only for norm == "2", but La_rcond_type() disallows
-                  ##     norm == "2" ... FIXME ??
-                  if(m < n) {
-                      x <- t(x)
-                      n <- m
-                  }
-                  R <- triu(qr(x)[["qr"]][seq_len(n), , drop = FALSE])
-                  rcond(R, norm = norm, ...)
-              }
-          })
-
-setMethod("rcond", signature(x = "dtrMatrix", norm = "character"),
-          function(x, norm, ...)
-              .Call(dtrMatrix_rcond, x, norm))
-
-setMethod("rcond", signature(x = "dtpMatrix", norm = "character"),
-          function(x, norm, ...)
-              .Call(dtpMatrix_rcond, x, norm))
-
-setMethod("rcond", signature(x = "dsyMatrix", norm = "character"),
-          function(x, norm, ...) {
-              trf <- BunchKaufman(x, warnSing = FALSE)
-              .Call(dsyMatrix_rcond, x, trf, norm)
-          })
-
-setMethod("rcond", signature(x = "dspMatrix", norm = "character"),
-          function(x, norm, ...) {
-              trf <- BunchKaufman(x, warnSing = FALSE)
-              .Call(dspMatrix_rcond, x, trf, norm)
-          })
-
-setMethod("rcond", signature(x = "dpoMatrix", norm = "character"),
-          function(x, norm, ...) {
-              ok <- TRUE
-              trf <- tryCatch(
-                  Cholesky(x, perm = FALSE),
-                  error = function(e) {
-                      ok <<- FALSE
-                      BunchKaufman(x, warnSing = FALSE)
-                  })
-              if(ok)
-                  .Call(dpoMatrix_rcond, x, trf, norm)
-              else .Call(dsyMatrix_rcond, x, trf, norm)
-          })
-
-setMethod("rcond", signature(x = "dppMatrix", norm = "character"),
-          function(x, norm, ...) {
-              ok <- TRUE
-              trf <- tryCatch(
-                  Cholesky(x, perm = FALSE),
-                  error = function(e) {
-                      ok <<- FALSE
-                      BunchKaufman(x, warnSing = FALSE)
-                  })
-              if(ok)
-                  .Call(dppMatrix_rcond, x, trf, norm)
-              else .Call(dspMatrix_rcond, x, trf, norm)
           })
--- pkg/Matrix/src/kappa.h
+++ pkg/Matrix/src/kappa.h
@@ -5,22 +5,17 @@
 #include "Mutils.h"
 
 SEXP dgeMatrix_norm(SEXP obj, SEXP type);
-SEXP dgeMatrix_rcond(SEXP obj, SEXP trf, SEXP type);
-
-SEXP dtrMatrix_norm(SEXP obj, SEXP type);
-SEXP dtrMatrix_rcond(SEXP obj, SEXP type);
-
-SEXP dtpMatrix_norm(SEXP obj, SEXP type);
-SEXP dtpMatrix_rcond(SEXP obj, SEXP type);
-
 SEXP dsyMatrix_norm(SEXP obj, SEXP type);
-SEXP dsyMatrix_rcond(SEXP obj, SEXP trf, SEXP type);
-
 SEXP dspMatrix_norm(SEXP obj, SEXP type);
+SEXP dtrMatrix_norm(SEXP obj, SEXP type);
+SEXP dtpMatrix_norm(SEXP obj, SEXP type);
+
+SEXP dgeMatrix_rcond(SEXP obj, SEXP trf, SEXP type);
+SEXP dsyMatrix_rcond(SEXP obj, SEXP trf, SEXP type);
 SEXP dspMatrix_rcond(SEXP obj, SEXP trf, SEXP type);
-
 SEXP dpoMatrix_rcond(SEXP obj, SEXP trf, SEXP type);
-
 SEXP dppMatrix_rcond(SEXP obj, SEXP trf, SEXP type);
+SEXP dtrMatrix_rcond(SEXP obj,           SEXP type);
+SEXP dtpMatrix_rcond(SEXP obj,           SEXP type);
 
 #endif /* MATRIX_KAPPA_H */
