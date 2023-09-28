671e7eea2637f54b118f88c28a5d9be5ab1115c1
jaganmn (Mikael Jagan) <jaganmn@mcmaster.ca>
2023-09-28 17:21:33 +0000

    unregister ancient dpoMatrix_chol, Csparse_diagU2N; other cosmetic
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/matrix@4388 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/Matrix/src/chm_common.h
+++ pkg/Matrix/src/chm_common.h
@@ -4,10 +4,10 @@
 #include "cholmod-etc.h"
 
 typedef cholmod_common  * CHM_CM;
-typedef cholmod_dense   * CHM_DN;
 typedef cholmod_factor  * CHM_FR;
 typedef cholmod_sparse  * CHM_SP;
 typedef cholmod_triplet * CHM_TR;
+typedef cholmod_dense   * CHM_DN;
 
 SEXP get_SuiteSparse_version(void);
 
@@ -16,18 +16,15 @@ SEXP CHM_set_common_env(SEXP);
 void CHM_store_common(void);
 void CHM_restore_common(void);
 
-CHM_SP as_cholmod_sparse   (CHM_SP, SEXP, Rboolean, Rboolean);
-CHM_TR as_cholmod_triplet  (CHM_TR, SEXP, Rboolean);
-CHM_DN as_cholmod_dense    (CHM_DN, SEXP);
 CHM_FR as_cholmod_factor   (CHM_FR, SEXP);
-CHM_FR as_cholmod_factor3  (CHM_FR, SEXP, Rboolean);
-CHM_DN numeric_as_chm_dense(CHM_DN, double *, int, int);
+CHM_SP as_cholmod_sparse   (CHM_SP, SEXP, Rboolean, Rboolean);
+CHM_DN as_cholmod_dense    (CHM_DN, SEXP);
 
 SEXP chm_factor_to_SEXP (CHM_FR, int);
 SEXP chm_sparse_to_SEXP (CHM_SP, int, int, int, const char *, SEXP);
-SEXP chm_triplet_to_SEXP(CHM_TR, int, int, int, const char *, SEXP);
 
 double chm_factor_ldetL2(CHM_FR);
 CHM_FR chm_factor_update(CHM_FR, CHM_SP, double);
+CHM_DN numeric_as_chm_dense(CHM_DN, double *, int, int);
 
 #endif /* MATRIX_CHM_COMMON_H */
