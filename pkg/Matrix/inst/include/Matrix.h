b4d1e83ee5a9a9056372b789bce958d1b921d6f9
jaganmn (Mikael Jagan) <jaganmn@mcmaster.ca>
2023-09-29 19:40:52 +0000

    remain backwards compatible with recursive reverse LinkingTo
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/matrix@4402 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/Matrix/inst/include/Matrix.h
+++ pkg/Matrix/inst/include/Matrix.h
@@ -36,14 +36,6 @@ extern void *alloca(size_t);
 # define attribute_hidden
 #endif
 
-#define MATRIX_VALID_ge_dense \
-"ngeMatrix", "lgeMatrix", "dgeMatrix"
-
-#define MATRIX_VALID_Csparse \
-"ngCMatrix", "lgCMatrix", "dgCMatrix", \
-"nsCMatrix", "lsCMatrix", "dsCMatrix", \
-"ntCMatrix", "ltCMatrix", "dtCMatrix"
-
 bool Matrix_isclass_ge_dense(SEXP);
 bool Matrix_isclass_Csparse (SEXP);
 
@@ -61,14 +53,31 @@ CHM_FR M_cholmod_factor_update(CHM_FR, CHM_SP, double);
 #define AS_CHM_FR(x) \
 	M_sexp_as_cholmod_factor((CHM_FR) alloca(sizeof(cholmod_factor)), x)
 
-#define AS_CHM_DN(x) \
-	M_sexp_as_cholmod_dense ((CHM_DN) alloca(sizeof(cholmod_dense )), x)
-
 #define AS_CHM_SP(x) \
 	M_sexp_as_cholmod_sparse((CHM_SP) alloca(sizeof(cholmod_sparse)), x, \
 	                         (Rboolean) 1, (Rboolean) 0)
 
-/* Non-(diag == "U")-checking version : */
+#define AS_CHM_DN(x) \
+	M_sexp_as_cholmod_dense ((CHM_DN) alloca(sizeof(cholmod_dense )), x)
+
+
+/* Below macros will be removed eventually, should not be used in new code : */
+
+#define M_as_cholmod_sparse  M_sexp_as_cholmod_sparse
+#define M_as_cholmod_dense   M_sexp_as_cholmod_dense
+#define M_chm_factor_to_SEXP M_cholmod_factor_as_sexp
+#define M_chm_sparse_to_SEXP M_cholmod_sparse_as_sexp
+#define M_chm_factor_ldetL2  M_cholmod_factor_ldetL2
+#define M_chm_factor_update  M_cholmod_factor_update
+
+#define MATRIX_VALID_ge_dense \
+"ngeMatrix", "lgeMatrix", "dgeMatrix"
+
+#define MATRIX_VALID_Csparse \
+"ngCMatrix", "lgCMatrix", "dgCMatrix", \
+"nsCMatrix", "lsCMatrix", "dsCMatrix", \
+"ntCMatrix", "ltCMatrix", "dtCMatrix"
+
 #define AS_CHM_SP__(x) \
 	M_sexp_as_cholmod_sparse((CHM_SP) alloca(sizeof(cholmod_sparse)), x, \
 	                         (Rboolean) 0, (Rboolean) 0)
