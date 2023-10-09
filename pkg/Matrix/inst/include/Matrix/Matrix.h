b34cbd619b8ae96bfd2058daa0addfeef7410f2a
jaganmn (Mikael Jagan) <jaganmn@mcmaster.ca>
2023-10-09 01:11:49 +0000

    nest API headers under inst/include/Matrix/
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/matrix@4428 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/Matrix/inst/include/Matrix/Matrix.h
@@ -0,0 +1,18 @@
+#ifndef R_MATRIX_MATRIX_H
+#define R_MATRIX_MATRIX_H
+
+#include "cholmod.h"
+
+#ifndef R_MATRIX_NO_CHOLMOD_UTILS
+# include "cholmod-utils.h"
+#endif
+
+#ifndef R_MATRIX_NO_ALLOCA
+# include "alloca.h"
+#endif
+
+#ifndef R_MATRIX_NO_REMAP
+# include "remap.h"
+#endif
+
+#endif /* R_MATRIX_MATRIX_H */
