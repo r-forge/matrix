cd4b9de503ffe9f812c5be2d4cdba0ea8fed30b9
jaganmn (Mikael Jagan) <jaganmn@mcmaster.ca>
2023-10-11 13:19:59 +0000

    because mcmcsae includes cholmod.h but not Matrix.h
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/matrix@4444 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/Matrix/inst/include/Matrix_stubs.c
+++ pkg/Matrix/inst/include/Matrix_stubs.c
@@ -1,3 +1,4 @@
 /* For backwards compatibility only.  Packages should start using */
 /* LinkingTo: Matrix (>= 1.6-2) and #include <Matrix/stubs.c>.    */
+#include "Matrix/alloca.c"
 #include "Matrix/stubs.c"
