218915d4ea6ee7dace5c4d4fbe1845fc7cba10d2
jaganmn (Mikael Jagan) <jaganmn@mcmaster.ca>
2023-11-05 20:45:48 +0000

    typo
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/matrix@4506 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- www/index.php
+++ www/index.php
@@ -18,7 +18,7 @@ echo '<?xml version="1.0" encoding="UTF-8"?>';
 
   <head>
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-	<title><The Matrix Project></title>
+	<title>The Matrix Project</title>
 	<link href="<?php echo $themeroot; ?>styles/estilo1.css" rel="stylesheet" type="text/css" />
   </head>
 
