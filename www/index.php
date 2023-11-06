363c28bdc9ac1a3e9b4e7d76ffc107bd75bfbf54
jaganmn (Mikael Jagan) <jaganmn@mcmaster.ca>
2023-11-06 14:35:59 +0000

    more refinement
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/matrix@4509 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- www/index.php
+++ www/index.php
@@ -37,7 +37,7 @@ echo '<?xml version="1.0" encoding="UTF-8"?>';
 <h1>The Matrix Project</h1>
 
 <p>
-This is the front page of <a href="https://r-forge.r-project.org/projects/matrix/">the Matrix project</a> on R-Forge, which is the home of R packages <a href="https://cran.r-project.org/package=Matrix"><b>Matrix</b></a> and <a href="https://cran.r-project.org/package=MatrixModels"><b>MatrixModels</b></a>.  These provide S4 classes and methods for representing, operating on, and modelling with sparse and dense matrices.
+Welcome to <a href="https://r-forge.r-project.org/projects/matrix/">the Matrix project</a> on R-Forge, home of R packages <a href="https://cran.r-project.org/package=Matrix"><b>Matrix</b></a> and <a href="https://cran.r-project.org/package=MatrixModels"><b>MatrixModels</b></a>.  These provide S4 classes and methods for representing, operating on, and modelling with sparse and dense matrices.  Since R version 2.9.0, <b>Matrix</b> has belonged to a list of "recommended" packages bundled in standard installations of R (in the default library, alongside <b>base</b>).  Hence it is typically possible to load a version of <b>Matrix</b> "out of the box" (with, as usual, <code>library(Matrix)</code>).
 </p>
 
 <h2>Installation</h2>
@@ -56,11 +56,7 @@ Older release versions are preserved in the CRAN <a href="https://cran.r-project
 </p>
 
 <p>
-Since R version 2.9.0, <b>Matrix</b> has belonged to a list of "recommended" packages bundled in standard installations of R, so that <code>library(Matrix)</code> works out of the box.  Still, one often installs <b>Matrix</b> from CRAN into a user library if the version in the default library is out of date.
-</p>
-
-<p>
-Note that <em>source</em> installation (as opposed to <em>binary</em> installation) requires compilers and additional tools; see the <a href="https://stat.ethz.ch/R-manual/R-patched/doc/manual/R-admin.html#Installing-packages"><i>R Installation and Administration</i></a> manual for platform-specific details.
+Note that <em>source</em> installation (as opposed to <em>binary</em> installation) requires compilers and other tools; see manual <a href="https://stat.ethz.ch/R-manual/R-patched/doc/manual/R-admin.html#Installing-packages"><i>R Installation and Administration</i></a> for platform-specific details.
 </p>
 
 <h2>Documentation</h2>
@@ -76,7 +72,7 @@ Indices of available help topics, data sets, and vignettes can be accessed with:
 </pre>
 
 <p>
-Passing <code>help_type = "html"</code> to <code>help</code> renders a hyperlinked version of the index in your browser.  Because <b>Matrix</b> is "recommended", its index is also hosted <a href="https://stat.ethz.ch/R-manual/R-patched/library/Matrix/html/00Index.html">here</a>.  (The version is that which is bundled in the latest release of R, which will be recent but need not be current.)
+Passing <code>help_type = "html"</code> to <code>help</code> renders a hyperlinked version of the index in your browser.  Because <b>Matrix</b> is "recommended", its index is also hosted <a href="https://stat.ethz.ch/R-manual/R-patched/library/Matrix/html/00Index.html">here</a>.  (The version is that which is bundled in the latest release of R.  It will be recent but need not be current.)
 </p>
 
 <p>
@@ -85,10 +81,10 @@ Doxygen-generated documentation for the C sources of <b>Matrix</b> is hosted <a 
 
 Slides for past talks about the Matrix project are hosted <a href="./slides">here</a>.
 
-<h2>Accessing the source code</h2>
+<h2>Browsing the source code</h2>
 
 <p>
-The Matrix project is maintained in a Subversion ("SVN") repository.  If you have installed Subversion, then you can check out the latest development version of <b>Matrix</b> with:
+The Matrix project is maintained in a Subversion ("SVN") repository.  If you have installed Subversion and have <code>svn</code> on your <code>PATH</code>, then you can check out the latest development version of <b>Matrix</b> with:
 </p>
 
 <pre>
@@ -96,7 +92,7 @@ $ svn checkout [-r REV] svn://svn.r-forge.r-project.org/svnroot/matrix/pkg/Matri
 </pre>
 
 <p>
-where <code>REV</code> and <code>DEST</code> optionally specify a revision number and destination path.  Without Subversion, one can browse the repository using R-Forge's <a href="https://r-forge.r-project.org/scm/viewvc.php/pkg/?root=matrix">ViewVC interface</a> or download a tarball produced by <code>R CMD build</code> from CRAN or R-Forge:
+where, optionally, <code>REV</code> and <code>DEST</code> specify a revision number and destination path.  Without Subversion, one can browse the repository using R-Forge's <a href="https://r-forge.r-project.org/scm/viewvc.php/pkg/?root=matrix">ViewVC interface</a> or download a tarball produced by <code>R CMD build</code> from CRAN or R-Forge:
 </p>
 
 <pre>
