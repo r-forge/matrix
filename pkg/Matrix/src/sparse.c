cab9215a44f8465b6f10aa489463827f5e8a724f
jaganmn (Mikael Jagan) <jaganmn@mcmaster.ca>
2023-12-11 01:57:56 +0000

    use paren in format string, as everywhere in validity.c; give -m, n in message, too
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/matrix@4536 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/Matrix/src/sparse.c
+++ pkg/Matrix/src/sparse.c
@@ -587,15 +587,15 @@ SEXP R_sparse_band(SEXP from, SEXP k1, SEXP k2)
 	if (k1 == R_NilValue) // tril()
 		a = -m ; // was (m > 0) ? 1 - m : 0;
 	else if ((a = asInteger(k1)) == NA_INTEGER || a < -m || a > n)
-		error(_("'%s'=%d must be an integer from %s to %s"),
-		      "k1", a, "-Dim[1]", "Dim[2]");
+		error(_("'%s' (%d) must be an integer from %s (%d) to %s (%d)"),
+		      "k1", a, "-Dim[1]", -m, "Dim[2]", n);
 	if (k2 == R_NilValue) // triu()
 		b = n; // was (n > 0) ? n - 1 : 0;
 	else if ((b = asInteger(k2)) == NA_INTEGER || b < -m || b > n)
-		error(_("'%s'=%d must be an integer from %s to %s"),
-		      "k2", b, "-Dim[1]", "Dim[2]");
+		error(_("'%s' (%d) must be an integer from %s (%d) to %s (%d)"),
+		      "k2", b, "-Dim[1]", -m, "Dim[2]", n);
 	else if (b < a)
-		error(_("'%s'=%d must be less than or equal to '%s'=%d"),
+		error(_("'%s' (%d) must be less than or equal to '%s' (%d)"),
 		      "k1", a, "k2", b);
 
 	return sparse_band(from, valid[ivalid], a, b);
