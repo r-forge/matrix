2555732fa60baecc9e2cb693a1276901797a29ed
jaganmn (Mikael Jagan) <jaganmn@mcmaster.ca>
2024-09-14 19:07:45 +0000

    remove uncompiled Matrix_mem*()
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/matrix@4918 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/Matrix/src/utils.h
+++ pkg/Matrix/src/utils.h
@@ -4,10 +4,6 @@
 #include <stddef.h> /* size_t */
 #include <Rinternals.h>
 
-#if 0 /* MJ: used for a while but not clearly needed */
-void *Matrix_memset(void *, int, R_xlen_t, size_t);
-void *Matrix_memcpy(void *, const void *, R_xlen_t, size_t);
-#endif
 char *Matrix_sprintf(const char *, ...);
 
 int equalString(SEXP, SEXP, R_xlen_t);
