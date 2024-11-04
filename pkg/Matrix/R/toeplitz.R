eeaac37628c6103df19bb4f2c73a34de2a7a002f
jaganmn (Mikael Jagan) <jaganmn@mcmaster.ca>
2024-11-04 14:47:19 +0000

    use '.V2sparse' instead of 'spV2M' for toeplitz
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/matrix@4977 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/Matrix/R/toeplitz.R
+++ pkg/Matrix/R/toeplitz.R
@@ -9,26 +9,18 @@ setMethod("toeplitz", c(x = "sparseVector"),
                   stop(gettextf("dimensions cannot exceed %s",
                                 "2^31-1"),
                        domain = NA)
-              nn <- c(n, n)
-              r <- spV2M(x[as.integer(abs(.col(nn) - .row(nn))) + 1L],
-                         nrow = n, ncol = n, symmetric = symmetric,
-                         check = FALSE)
-              repr <- # keep in sync with sparseMatrix
-              if (missing(giveCsparse))
+              repr <-
+              if (!missing(repr) || missing(giveCsparse))
                   match.arg(repr)
-              else if (!missing(repr)) {
-                  warning(gettextf("'%s' is deprecated; using '%s' instead",
-                                   "giveCsparse", "repr"),
-                          domain = NA)
-                  match.arg(repr)
-              }
               else if (giveCsparse)
                   "C"
-              else {
+              else "T"
+              if (!missing(giveCsparse))
                   warning(gettextf("'%s' is deprecated; using %s=\"%s\"",
-                                   "giveCsparse", "repr", "T"),
+                                   "giveCsparse", "repr", repr),
                           domain = NA)
-                  "T"
-              }
-              switch(repr, "C" = .M2C(r), "R" = .M2R(r), "T" = r)
+              d <- c(n, n)
+              .V2sparse(x[as.integer(abs(.col(d) - .row(d))) + 1L],
+                        paste0(".", if (symmetric) "s" else "g", repr),
+                        uplo = "U", trans = "T", nrow = n, ncol = n)
           })
