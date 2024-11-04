39672da1cbf8382dad6f89acf52b977bd423784e
jaganmn (Mikael Jagan) <jaganmn@mcmaster.ca>
2024-11-04 14:43:30 +0000

    simplify 'updown' dispatch
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/matrix@4974 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/Matrix/R/updown.R
+++ pkg/Matrix/R/updown.R
@@ -9,29 +9,21 @@ setMethod("updown",
 setMethod("updown",
           c(update = "logical", C = "Matrix", L = "sparseCholesky"),
           function(update, C, L)
-              updown(update, .M2kind(.M2C(C), ","), L))
+              updown(update, .M2C(C), L))
 
 setMethod("updown",
           c(update = "logical", C = "matrix", L = "sparseCholesky"),
           function(update, C, L)
               updown(update, .m2sparse(C, ",gC"), L))
 
-for (.cl in c("dgCMatrix", "dsCMatrix"))
 setMethod("updown",
-          c(update = "logical", C = .cl, L = "sparseCholesky"),
+          c(update = "logical", C = "CsparseMatrix", L = "sparseCholesky"),
           function(update, C, L) {
+              C <- .M2kind(C, ",")
               if (length(perm <- L@perm))
                   C <- C[perm + 1L, , drop = FALSE]
-              .Call(sparseCholesky_updown, L, C, update)
-          })
-
-setMethod("updown",
-          c(update = "logical", C = "dtCMatrix", L = "sparseCholesky"),
-          function(update, C, L) {
-              if (C@diag != "N")
+              if (.M.shape(C) == "t" && C@diag != "N")
                   C <- ..diagU2N(C)
-              if (length(perm <- L@perm))
-                  C <- C[perm + 1L, , drop = FALSE]
               .Call(sparseCholesky_updown, L, C, update)
           })
 
@@ -49,12 +41,7 @@ function(object, parent, mult = 0)
 setMethod("update", c(object = "sparseCholesky"),
           function(object, parent, mult = 0, ...) {
               parent <- .M2kind(.M2C(parent), ",")
-              if ((shape <- .M.shape(parent)) != "s") {
-                  Matrix.message(gettextf("'%1$s' is not formally symmetric; factorizing tcrossprod(%1$s)",
-                                          "parent"),
-                                 domain = NA)
-                  if (shape == "t" && parent@diag != "N")
-                      parent <- ..diagU2N(parent)
-              }
+              if (.M.shape(parent) == "t" && parent@diag != "N")
+                  parent <- ..diagU2N(parent)
               .Call(sparseCholesky_update, object, parent, mult)
           })
