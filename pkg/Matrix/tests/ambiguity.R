56dbbf12829ed062e14acc0f4c659a43875333d8
jaganmn (Mikael Jagan) <jaganmn@mcmaster.ca>
2024-11-05 21:01:05 +0000

    start testing for unambiguous method dispatch
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/matrix@4997 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/Matrix/tests/ambiguity.R
@@ -0,0 +1,19 @@
+library(Matrix)
+if (requireNamespace("SparseM", quietly = TRUE))
+    library(SparseM)
+if (requireNamespace("graph", quietly = TRUE))
+    library(graph)
+
+ns <- asNamespace("Matrix")
+generics <- getGenerics(ns)
+l <- sapply(generics, testInheritedMethods, simplify = FALSE)
+
+(namb <- vapply(l, function(x) length(x@target), 0L))
+(todo <- names(namb)[namb > 0L])
+
+## Make sure that we are removing from and never adding to this
+## list of generic functions with ambiguous method dispatch :
+todo. <- c("%%", "%/%", "&", "*", "+", "-", "/",
+           "Arith", "Compare", "Logic", "Ops",
+           "[<-", "^", "crossprod", "tcrossprod")
+stopifnot(identical(todo, todo.))
