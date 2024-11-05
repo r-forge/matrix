b61e26aa962bdff3cb82b15063583a44b1ea32ee
jaganmn (Mikael Jagan) <jaganmn@mcmaster.ca>
2024-11-05 16:05:47 +0000

    non-breaking version of r4988
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/matrix@4994 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/Matrix/R/norm.R
+++ pkg/Matrix/R/norm.R
@@ -10,7 +10,7 @@ setMethod("norm", c(x = "denseMatrix", type = "character"),
               if (identical(type, "2"))
                   return(base::norm(.M2m(x), type = "2"))
               x <- .M2kind(x, ",")
-              switch(substr(.M.class(x), 2L, 3L),
+              switch(EXPR = substr(.M.class(x), 2L, 3L),
                      "ge" = .Call(geMatrix_norm, x, type),
                      "sy" = .Call(syMatrix_norm, x, type),
                      "sp" = .Call(spMatrix_norm, x, type),
@@ -22,11 +22,7 @@ setMethod("norm", c(x = "sparseMatrix", type = "character"),
           function(x, type, ...) {
               if (any(x@Dim == 0L))
                   return(0)
-              switch(EXPR = type[1L],
-                     "O" =, "o" =, "1" =
-                         max(colSums(abs(x))),
-                     "I" =, "i" =
-                         max(rowSums(abs(x))),
+              switch(EXPR = type,
                      "2" =
                          {
                              warning(gettextf("'%s' via sparse -> dense coercion",
@@ -35,7 +31,11 @@ setMethod("norm", c(x = "sparseMatrix", type = "character"),
                              base::norm(.M2m(x), type = "2")
                          },
                      "M" =, "m" =
-                         max(abs(x)),
+                         max(abs(range(x))),
+                     "O" =, "o" =, "1" =
+                         max(colSums(abs(x))),
+                     "I" =, "i" =
+                         max(rowSums(abs(x))),
                      "F" =, "f" =, "E" =, "e" =
                          {
                              if (.M.kind(x) == "z")
@@ -43,7 +43,7 @@ setMethod("norm", c(x = "sparseMatrix", type = "character"),
                              sqrt(sum(x * x))
                          },
                      stop(gettextf("invalid %s=\"%s\"",
-                                   "type", type[1L]),
+                                   "type", type),
                           domain = NA))
           })
 
@@ -56,12 +56,12 @@ setMethod("norm", c(x = "diagonalMatrix", type = "character"),
                   if (.M.kind(x) == "n" && anyNA(y))
                       y <- y | is.na(y)
               }
-              switch(EXPR = type[1L],
-                     "O" =, "o" =, "1" =,
-                     "I" =, "i" =,
+              switch(EXPR = type,
                      "2" =,
-                     "M" =, "m" =
-                         if (nonunit) max(abs(y)) else 1,
+                     "M" =, "m" =,
+                     "O" =, "o" =, "1" =,
+                     "I" =, "i" =
+                         if (nonunit) max(abs(range(y))) else 1,
                      "F" =, "f" =, "E" =, "e" =
                          if (nonunit) {
                              if (is.complex(y))
@@ -69,7 +69,7 @@ setMethod("norm", c(x = "diagonalMatrix", type = "character"),
                              sqrt(sum(y * y))
                          } else sqrt(n),
                      stop(gettextf("invalid %s=\"%s\"",
-                                   "type", type[1L]),
+                                   "type", type),
                           domain = NA))
           })
 
@@ -78,19 +78,19 @@ setMethod("norm", c(x = "indMatrix", type = "character"),
               d <- x@Dim
               if ((m <- d[1L]) == 0L || (n <- d[2L]) == 0L)
                   return(0)
-              switch(EXPR = type[1L],
-                     "O" =, "o" =, "1" =
-                         if (x@margin == 1L) max(tabulate(x@perm, n)) else 1,
-                     "I" =, "i" =
-                         if (x@margin == 1L) 1 else max(tabulate(x@perm, m)),
+              switch(EXPR = type,
                      "2" =
                          sqrt(max(tabulate(x@perm, if (x@margin == 1L) n else m))),
                      "M" =, "m" =
                          1,
+                     "O" =, "o" =, "1" =
+                         if (x@margin == 1L) max(tabulate(x@perm, n)) else 1,
+                     "I" =, "i" =
+                         if (x@margin == 1L) 1 else max(tabulate(x@perm, m)),
                      "F" =, "f" =, "E" =, "e" =
                          if (x@margin == 1L) sqrt(m) else sqrt(n),
                      stop(gettextf("invalid %s=\"%s\"",
-                                   "type", type[1L]),
+                                   "type", type),
                           domain = NA))
           })
 
@@ -98,15 +98,15 @@ setMethod("norm", c(x = "pMatrix", type = "character"),
           function(x, type, ...) {
               if ((n <- x@Dim[1L]) == 0L)
                   return(0)
-              switch(EXPR = type[1L],
-                     "O" =, "o" =, "1" =,
-                     "I" =, "i" =,
+              switch(EXPR = type,
                      "2" =,
-                     "M" =, "m" =
+                     "M" =, "m" =,
+                     "O" =, "o" =, "1" =,
+                     "I" =, "i" =
                          1,
                      "F" =, "f" =, "E" =, "e" =
                          sqrt(n),
                      stop(gettextf("invalid %s=\"%s\"",
-                                   "type", type[1L]),
+                                   "type", type),
                           domain = NA))
           })
