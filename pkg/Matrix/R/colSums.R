d3914001a0222ece42aa0e8d13f9917c1a481f06
jaganmn (Mikael Jagan) <jaganmn@mcmaster.ca>
2024-11-12 16:30:03 +0000

    use 'updateBody' for methods for diagonalMatrix
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/matrix@5024 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/Matrix/R/colSums.R
+++ pkg/Matrix/R/colSums.R
@@ -1,8 +1,6 @@
 ## METHODS FOR GENERIC: colSums, colMeans, rowSums, rowMeans
 ## ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-## ==== denseMatrix ====================================================
-
 setMethod("colSums",  c(x = "denseMatrix"),
           function(x, na.rm = FALSE, dims = 1L, ...)
               .Call(R_dense_marginsum, x, 2L, na.rm, FALSE))
@@ -15,11 +13,6 @@ setMethod("rowSums",  c(x = "denseMatrix"),
 setMethod("rowMeans", c(x = "denseMatrix"),
           function(x, na.rm = FALSE, dims = 1L, ...)
               .Call(R_dense_marginsum, x, 1L, na.rm,  TRUE))
-
-
-## ==== sparseMatrix ===================================================
-
-## ---- [CRT]sparseMatrix ----------------------------------------------
 
 for (.cl in paste0(c("C", "R", "T"), "sparseMatrix")) {
 setMethod("colSums",  c(x = .cl),
@@ -35,16 +28,14 @@ setMethod("rowMeans", c(x = .cl),
           function(x, na.rm = FALSE, dims = 1L, sparseResult = FALSE, ...)
               .Call(R_sparse_marginsum, x, 1L, na.rm,  TRUE, sparseResult))
 }
-rm(.cl)
 
-
-## ---- diagonalMatrix -------------------------------------------------
-
-.diag.cS <- .diag.rS <- function(x, na.rm = FALSE, dims = 1L, ...) {
+.marSums.diagonal <-
+function(x, na.rm = FALSE, dims = 1L, ...) {
     kind <- .M.kind(x)
-    if ((n <- x@Dim[1L]) == 0L)
+    n <- x@Dim[1L]
+    if (n == 0L)
         return(vector(switch(kind, "z" = "complex", "d" = , "i" = "double", "integer"), 0L))
-    else if (x@diag != "N")
+    if (x@diag != "N")
         r <- rep.int(switch(kind, "z" = 1+0i, "d" = , "i" = 1, 1L), n)
     else {
         r <- switch(kind, "z" = , "d" = x@x, "i" = as.double(x@x), as.integer(x@x))
@@ -55,39 +46,38 @@ rm(.cl)
         names(r) <- nms
     r
 }
-body(.diag.cS) <- do.call(substitute, list(body(.diag.cS), list(.MARGIN = 2L)))
-body(.diag.rS) <- do.call(substitute, list(body(.diag.rS), list(.MARGIN = 1L)))
-
-.diag.cM <- .diag.rM <- function(x, na.rm = FALSE, dims = 1L, ...) {
+.marMeans.diagonal <-
+function(x, na.rm = FALSE, dims = 1L, ...) {
     kind <- .M.kind(x)
-    if ((n <- x@Dim[1L]) == 0L)
+    n <- x@Dim[1L]
+    if (n == 0L)
         return(vector(switch(kind, "z" = "complex", "double"), 0L))
     else if (x@diag != "N")
         r <- rep.int(switch(kind, "z" = 1+0i, 1) / n, n)
     else {
         r <- x@x / n
         if ((na.rm || kind == "n") && anyNA(r))
-            r[is.na(r)] <- switch(kind,
-                                  "z" = if (n == 1L) NaN * (0+0i) else 0+0i,
-                                  "n" = 1 / n,
-                                  if (n == 1L) NaN else 0)
+            r[is.na(r)] <-
+                switch(kind,
+                       "z" = if (n == 1L) NaN * (0+0i) else 0+0i,
+                       "n" = 1 / n,
+                       if (n == 1L) NaN else 0)
     }
     if (!is.null(nms <- x@Dimnames[[.MARGIN]]))
         names(r) <- nms
     r
 }
-body(.diag.cM) <- do.call(substitute, list(body(.diag.cM), list(.MARGIN = 2L)))
-body(.diag.rM) <- do.call(substitute, list(body(.diag.rM), list(.MARGIN = 1L)))
 
-setMethod("colSums",  c(x = "diagonalMatrix"), .diag.cS)
-setMethod("colMeans", c(x = "diagonalMatrix"), .diag.cM)
-setMethod("rowSums",  c(x = "diagonalMatrix"), .diag.rS)
-setMethod("rowMeans", c(x = "diagonalMatrix"), .diag.rM)
+setMethod("colSums",  c(x = "diagonalMatrix"),
+          updateBody(.marSums.diagonal, .MARGIN = 2L))
+setMethod("colMeans", c(x = "diagonalMatrix"),
+          updateBody(.marMeans.diagonal, .MARGIN = 2L))
+setMethod("rowSums",  c(x = "diagonalMatrix"),
+          updateBody(.marSums.diagonal, .MARGIN = 1L))
+setMethod("rowMeans", c(x = "diagonalMatrix"),
+          updateBody(.marMeans.diagonal, .MARGIN = 1L))
 
-rm(.diag.cS, .diag.cM, .diag.rS, .diag.rM)
-
-
-## ---- indMatrix (incl. pMatrix) --------------------------------------
+rm(.marSums.diagonal, .marMeans.diagonal)
 
 setMethod("colSums",  c(x = "indMatrix"),
           function(x, na.rm = FALSE, dims = 1L, ...) {
@@ -129,3 +119,5 @@ setMethod("rowMeans",  c(x = "indMatrix"),
                   names(r) <- nms
               r
           })
+
+rm(.cl)
