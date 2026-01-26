a1c88c13e2e9a44269f281fbf0352e8cc9f855cc
jaganmn (Mikael Jagan) <jaganmn@mcmaster.ca>
2026-01-26 03:25:15 +0000

    tweaks incl. not materializing unit diagonal
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/matrix@5184 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/Matrix/R/forceDiagonal.R
+++ pkg/Matrix/R/forceDiagonal.R
@@ -6,13 +6,17 @@ setMethod("forceDiagonal", c(x = "matrix"),
               d <- dim(x)
               if (d[1L] != d[2L])
                   stop("matrix is not square")
-              diax <- diag(x, names = FALSE)
-              if(is.null(diag))
-                  diag <- if(!anyNA(diax) && all(diax == as1(x[0L]))) "U" else "N"
-              r <- new(paste0(.M.kind(x), "diMatrix"), Dim = d,
-                       x = if (diag == "N") diax else x[0L], diag = diag)
-              if(!is.null(dn <- dimnames(x)))
+              if (is.null(diag) || diag == "N")
+                  y <- diag(x, names = FALSE)
+              if (is.null(diag))
+                  diag <- if (anyNA(y) || any(y != 1L)) "N" else "U"
+              r <- new(paste0(.M.kind(x), "diMatrix"))
+              r@Dim <- d
+              if (!is.null(dn <- dimnames(x)))
                   r@Dimnames <- dn
+              if (diag == "N")
+                  r@x <- y
+              else r@diag <- "U"
               r
           })
 
@@ -22,14 +26,11 @@ setMethod("forceDiagonal", c(x = .cl),
               d <- x@Dim
               if (d[1L] != d[2L])
                   stop("matrix is not square")
-              dn <- dimnames(x)
-              diag <-
-                  if (is.null(diag))
-                      (if (.M.shape(x) != "t") "N" else x@diag)
-                  else if (diag == "N") "N" else "U"
+              if (is.null(diag))
+                  diag <- if (.M.shape(x) != "t") "N" else x@diag
               r <- new(paste0(.M.kind(x), "diMatrix"))
               r@Dim <- d
-              r@Dimnames <- dn
+              r@Dimnames <- dimnames(x)
               if (diag == "N")
                   r@x <- diag(x, names = FALSE)
               else r@diag <- "U"
