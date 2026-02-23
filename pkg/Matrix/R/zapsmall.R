8c923d48458719fe6db1ae74fbda879ac3db06ec
jaganmn (Mikael Jagan) <jaganmn@mcmaster.ca>
2026-02-23 20:36:09 +0000

    fix 'zapsmall' method signatures
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/matrix@5190 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/Matrix/R/zapsmall.R
+++ pkg/Matrix/R/zapsmall.R
@@ -4,17 +4,21 @@
 ## MJ: not quite optimal as we lose diag="U" even if 1 is not zapped
 
 setMethod("zapsmall", signature(x = "Matrix"),
-          function(x, ...) {
+          function(x, digits = getOption("digits"),
+                   mFUN = function(x, ina) max(abs(x[!ina])),
+                   min.d = 0L, ...) {
               r <- .M2kind(forceCanonical(.promote(x), diag = "N"), ",")
-              r@x <- zapsmall(x@x, ...)
+              r@x <- zapsmall(x@x, digits = digits, mFUN = mFUN, min.d = min.d)
               if (.M.shape(r) != "t")
                   r@factors <- list()
               r
           })
 
 setMethod("zapsmall", signature(x = "sparseVector"),
-          function(x, ...) {
+          function(x, digits = getOption("digits"),
+                   mFUN = function(x, ina) max(abs(x[!ina])),
+                   min.d = 0L, ...) {
               r <- .V2kind(.promote(x), ",")
-              r@x <- zapsmall(x@x, ...)
+              r@x <- zapsmall(x@x, digits = digits, mFUN = mFUN, min.d = min.d)
               r
           })
