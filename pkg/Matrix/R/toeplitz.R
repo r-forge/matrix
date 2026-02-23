10f2b4a3cefac8339c0be559691885161b729669
jaganmn (Mikael Jagan) <jaganmn@mcmaster.ca>
2026-02-23 20:42:52 +0000

    toeplitz(x=, r=) for asymmetric Toeplitz
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/matrix@5192 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/Matrix/R/toeplitz.R
+++ pkg/Matrix/R/toeplitz.R
@@ -2,13 +2,23 @@
 ## ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 setMethod("toeplitz", c(x = "sparseVector"),
-          function(x, symmetric = TRUE, repr = c("C", "R", "T"),
-                   giveCsparse, ...) {
-              n <- length(x)
-              if (n > .Machine[["integer.max"]])
+          function(x, r = NULL, symmetric = is.null(r),
+                   repr = c("C", "R", "T"), giveCsparse, ...) {
+              m <- n <- length(x)
+              if (!symmetric && !is.null(r)) {
+                  stopifnot(is(r, "sparseVector"))
+                  n <- length(r)
+              }
+              if (is.double(m) || is.double(n))
                   stop(gettextf("dimensions cannot exceed %s",
                                 "2^31-1"),
                        domain = NA)
+              d <- c(m, n)
+              y <-
+              if (symmetric || is.null(r))
+                  x[as.integer(abs(.row(d) - .col(d))) + 1L]
+              else c(r[if (n >= 2L) n:2L else 0L], x)[.row(d) - .col(d) + n]
+              
               repr <-
               if (!missing(repr) || missing(giveCsparse))
                   match.arg(repr)
@@ -19,8 +29,6 @@ setMethod("toeplitz", c(x = "sparseVector"),
                   warning(gettextf("'%s' is deprecated; using %s=\"%s\"",
                                    "giveCsparse", "repr", repr),
                           domain = NA)
-              d <- c(n, n)
-              .V2sparse(x[as.integer(abs(.col(d) - .row(d))) + 1L],
-                        paste0(".", if (symmetric) "s" else "g", repr),
-                        uplo = "U", trans = "T", nrow = n, ncol = n)
+              .V2sparse(y, paste0(".", if (symmetric) "s" else "g", repr),
+                        uplo = "U", trans = "T", nrow = m, ncol = n)
           })
